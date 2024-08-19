96173009f247d1ac8a368d806863bca868f01d3d
wei756 <wei756fg@gmail.com>
2024-08-20 01:54:45 +0900

    chore: scss 절대 경로 import 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import path from 'path';
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/app/')],
+  },
+};
 
 export default nextConfig;

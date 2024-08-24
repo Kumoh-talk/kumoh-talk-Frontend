bfddb8349d5cb54fa91bfa04d0e41b428015d484
Hwangyongjin <dswvgw6412@naver.com>
2024-08-24 22:37:04 +0900

    feat: 댓글 메뉴 UI 구현

---
+++ src/app/assets/svg/MoreSvg.tsx
@@ -0,0 +1,16 @@
+export default function MoreSvg() {
+  return (
+    <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M14 15C14.5523 15 15 14.5523 15 14C15 13.4477 14.5523 13 14 13C13.4477 13 13 13.4477 13 14C13 14.5523 13.4477 15 14 15Z"
+        fill="#959595" stroke="#959595" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path
+        d="M14 7C14.5523 7 15 6.55228 15 6C15 5.44772 14.5523 5 14 5C13.4477 5 13 5.44772 13 6C13 6.55228 13.4477 7 14 7Z"
+        fill="#959595" stroke="#959595" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path
+        d="M14 23C14.5523 23 15 22.5523 15 22C15 21.4477 14.5523 21 14 21C13.4477 21 13 21.4477 13 22C13 22.5523 13.4477 23 14 23Z"
+        fill="#959595" stroke="#959595" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+
+  )
+}

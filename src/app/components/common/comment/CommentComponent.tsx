3035192cd19a6f604b779c3e38e6a782b57f3e4a
oko_jin <dswvgw6412@naver.com>
2024-09-21 23:43:24 +0900

    feat: useInput 적용

--- src/app/components/common/comment/CommentComponent.tsx
+++ src/app/components/common/comment/CommentComponent.tsx
@@ -1,18 +1,25 @@
 "use client"
 
-// TODO: 백엔드 api 완성되면 수정
 import CommentProfile from '@/app/components/common/comment/CommentProfile';
 import styles from './comment.module.scss';
 import CommentInput from '@/app/components/common/comment/CommentInput';
-import Comment from '@/app/components/common/comment/Comment';
+import Comment from '@/app/components/common/comment/Comment'
+import useInput from '@/app/lib/hooks/useInput';
+import { KeyboardEvent } from 'react';
 // TODO: 백엔드 api 완성되면 수정
 export default function CommentComponent() {
+  const [content, onChange] = useInput();
+
+  const onSubmit = (e: KeyboardEvent<HTMLInputElement>) => {
+    console.log(content);
+  }
+
   return (
     <div className={styles.block}>
       <div className={styles.head}>댓글 2</div>
       <div className={styles.inputBlock}>
-        <CommentProfile/>
-        <CommentInput placeholder={'댓글 추가'}/>
+        <CommentProfile />
+        <CommentInput placeholder={'댓글 추가'} name={'comment'} onChange={onChange} onKeyDown={onSubmit} />
       </div>
       <Comment name={'황용진'} date={'2024.08.24'} comment={'테스트 댓글이요'} />
     </div>
--- src/app/components/common/comment/CommentInput.tsx
+++ src/app/components/common/comment/CommentInput.tsx
@@ -1,11 +1,15 @@
 import styles from './comment.module.scss'
+import { ChangeEvent, KeyboardEvent } from 'react';
 
 export interface Props {
   placeholder: string;
+  name: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown?: (e: KeyboardEvent<HTMLInputElement>) => void;
 }
 
-export default function CommentInput({placeholder}: Props) {
+export default function CommentInput({ placeholder, name , onChange = (e) => {}, onKeyDown = (e) => {} }: Props) {
   return (
-    <input className={styles.input} placeholder={placeholder}/>
+    <input className={styles.input} placeholder={placeholder} name={name} onChange={onChange} onKeyDown={onKeyDown} />
   )
 }

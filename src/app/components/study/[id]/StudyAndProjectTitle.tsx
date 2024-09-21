46777a936594b02b07c5c18f05e3f2da011d0b42
oko_jin <dswvgw6412@naver.com>
2024-09-21 16:30:14 +0900

    chore: 각 내용 별로 Props를 받고 UI에 반영

--- src/app/components/study/[id]/StudyAndProjectTitle.tsx
+++ src/app/components/study/[id]/StudyAndProjectTitle.tsx
@@ -1,14 +1,19 @@
 import styles from './studyAndProjectTitle.module.scss';
 import HashTag from "@/app/components/study/[id]/HashTag";
+
+export interface Props {
+  title: string,
+  type: string,
+  tag: string,
+}
 // TODO: 백엔드 api 완성되면 수정
-export function StudyAndProjectTitle() {
+export function StudyAndProjectTitle({ title, type, tag }: Props) {
   return (
     <div className={styles.titleBlock}>
-      <div className={styles.title}>제목</div>
+      <div className={styles.title}>{title}</div>
       <div className={styles.tags}>
-        <HashTag />
-        <HashTag />
-        <HashTag />
+        <HashTag type={type} />
+        <HashTag type={tag} />
       </div>
     </div>
   )
--- src/app/study/[id]/page.tsx
+++ src/app/study/[id]/page.tsx
@@ -1,23 +1,24 @@
-import {StudyAndProjectTitle} from "@/app/components/study/[id]/StudyAndProjectTitle";
 import styles from './page.module.scss';
-import StudyAndProjectContent from "@/app/components/study/[id]/StudyAndProjectContent";
-import ApplyButton from "@/app/components/study/[id]/ApplyButton";
-import ModifyButton from "@/app/components/study/[id]/ModifyButton";
-import Comment from "@/app/components/common/comment/CommentComponent";
+import ApplyButton from '@/app/components/study/[id]/ApplyButton';
+import ModifyButton from '@/app/components/study/[id]/ModifyButton';
+import Comment from '@/app/components/common/comment/CommentComponent';
+import { Suspense } from 'react';
+import StudyAndProjectDetail from '@/app/components/study/[id]/StudyAndProjectDetail';
 
 // TODO: 백엔드 api 완성되면 수정
 export default function Page() {
   return (
     <main className={styles.board}>
-      <StudyAndProjectTitle/>
-      <StudyAndProjectContent/>
+      <Suspense fallback={<p>Loading...</p>}>
+        <StudyAndProjectDetail />
+      </Suspense>
       <div className={styles.buttonBlock}>
         <ApplyButton/>
       </div>
       <div className={styles.buttonBlock}>
         <ModifyButton/>
       </div>
-      <Comment />
+      <Comment/>
     </main>
   )
 }

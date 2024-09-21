4996055110af7ae88ec589afbd926dc2908929b3
oko_jin <dswvgw6412@naver.com>
2024-09-21 16:29:27 +0900

    feat: 수정하기 버튼 라우팅 추가

--- src/app/components/study/[id]/ModifyButton.tsx
+++ src/app/components/study/[id]/ModifyButton.tsx
@@ -1,13 +1,19 @@
 'use client'
 
 import styles from './boardButton.module.scss'
+import Button from '@/app/components/common/button/Button';
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
 // TODO: 백엔드 api 완성되면 수정
 export default function ModifyButton() {
-  const onModify = () => {};
+  const params = useParams();
+
+  const onModify = () => {
+  };
 
   return (
-    <button className={styles.button} onClick={onModify}>
-      수정하기
-    </button>
+    <Link style={{ textDecoration: 'none' }} href={`./modify?id=${params.id}`} passHref={true}>
+      <Button onClick={onModify} size={'medium'}>수정하기</Button>
+    </Link>
   )
 }

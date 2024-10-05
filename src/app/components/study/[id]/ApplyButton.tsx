338971a46c5849d557f2c3e7cebc252cf3503da0
oko_jin <dswvgw6412@naver.com>
2024-10-05 16:25:30 +0900

    chore: onApply 함수 추가

--- src/app/components/study/[id]/ApplyButton.tsx
+++ src/app/components/study/[id]/ApplyButton.tsx
@@ -1,13 +1,19 @@
 'use client'
 
 import styles from './boardButton.module.scss';
+import Button from '@/app/components/common/button/Button';
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
 // TODO: 백엔드 api 완성되면 수정
 export default function ApplyButton() {
-  const onApply = () => {};
+  const params = useParams();
+
+  const onApply = () => {
+  };
 
   return (
-    <button className={styles.button} onClick={onApply}>
-      신청하기
-    </button>
+    <Link style={{ textDecoration: 'none' }} href={`./apply?id=${params.id}`} passHref={true}>
+      <Button onClick={ApplyButton} size={'medium'}>신청하기</Button>
+    </Link>
   )
 }

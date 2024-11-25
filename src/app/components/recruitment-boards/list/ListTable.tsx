afb67764d2071efb355a3793bfe33bf7b82863c0
oko_jin <dswvgw6412@naver.com>
2024-11-25 16:21:53 +0900

    feat: 게시물 별 신청자 리스트 데이터를 가져오는 useGetApplyList 구현

--- src/app/components/recruitment-boards/list/ListTable.tsx
+++ src/app/components/recruitment-boards/list/ListTable.tsx
@@ -1,18 +1,31 @@
+'use client';
+
 import ListTitle from '@/app/components/recruitment-boards/list/ListTitle';
 import ListContent from '@/app/components/recruitment-boards/list/ListContent';
 import styles from './listTable.module.scss';
+import { useSearchParams } from 'next/navigation';
+import useGetApplyList from '@/app/lib/hooks/useGetApplyList';
+import { ApplyListApi } from '@/app/lib/types/recruitmentBoards/applyList';
 
-export default function ListTable() {
+export default async function ListTable() {
+  const query = useSearchParams();
+  const boardId: string = query.get('id') as string;
+  const page: string = query.get('page') as string;
+  const sort: 'createdAt' | 'desc' = query.get('sort') as 'createdAt' | 'desc';
+  const { success, data }: ApplyListApi = await useGetApplyList({ boardId, page, sort });
+
+  if (!data.applicantList) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className={styles.list}>
-      <ListTitle />
-      <ListContent />
-      <ListContent />
-      <ListContent />
-      <ListContent />
-      <ListContent />
-      <ListContent />
-      <ListContent />
+      <ListTitle/>
+      <ListContent/>
+      <ListContent/>
+      {data.applicantList.map((applicant) => (
+        <ListContent key={applicant.applicantId}/>
+      ))}
     </div>
   )
 }

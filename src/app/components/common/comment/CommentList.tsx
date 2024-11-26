9e5ddd52cd871a806ba0b8de2faea11acefec4bc
oko_jin <dswvgw6412@naver.com>
2024-11-26 20:53:10 +0900

    chore: 댓글 컴포넌트 간 간격 조정

--- src/app/components/common/comment/CommentList.tsx
+++ src/app/components/common/comment/CommentList.tsx
@@ -4,6 +4,7 @@ import Comment from '@/app/components/common/comment/Comment';
 import { CommentListApi } from '@/app/lib/types/comment/commentList';
 import { useContext } from 'react';
 import { CommentListContext } from '@/app/components/common/comment/CommentListProvider';
+import styles from './comment.module.scss';
 
 export default function CommentList() {
   const { success, data }: CommentListApi = useContext(CommentListContext)
@@ -13,11 +14,11 @@ export default function CommentList() {
   }
 
   return (
-    <>
+    <div className={styles.commentList}>
       {data.commentInfoResponseList.map((comment) => (
         <Comment id={comment.commentId} key={comment.commentId} name={comment.userNickname} date={comment.createdAt}
                  comment={comment.content}/>
       ))}
-    </>
+    </div>
   )
 }

d9f166c5a0623c909f011ca52b8d6495a1b4018b
oko_jin <dswvgw6412@naver.com>
2024-11-26 14:10:02 +0900

    chore: 날짜가 형식에 맞게 나오도록 포맷팅

--- src/app/components/common/comment/Comment.tsx
+++ src/app/components/common/comment/Comment.tsx
@@ -5,6 +5,7 @@ import styles from './comment.module.scss'
 import MoreButton from '@/app/components/common/comment/MoreButton';
 import { useEffect, useRef, useState } from 'react';
 import useCommentEdit from '@/app/lib/hooks/useCommentEdit';
+import dayjs from 'dayjs';
 
 export interface Props {
   id: number;
@@ -17,7 +18,7 @@ export default function Comment({ id, name, date, comment }: Props) {
   const [isEdit, setIsEdit] = useState(false);
   const [content, onChange, onSubmit] = useCommentEdit({ id, comment, setIsEdit });
   const editRef: any = useRef(null);
-  
+
   useEffect(() => {
     if (isEdit) {
       editRef.current?.focus();
@@ -30,19 +31,20 @@ export default function Comment({ id, name, date, comment }: Props) {
       <div className={styles.commentMain}>
         <div className={styles.commentTop}>
           <div className={styles.commentName}>{name}</div>
-          <div className={styles.commentDate}>{date}</div>
+          <div className={styles.commentDate}>{dayjs(date).format('YYYY.MM.DD')}</div>
         </div>
         <div className={styles.commentBottom}>
           {
             isEdit ?
-              <input className={styles.commentEdit} ref={editRef} value={content} onChange={onChange} onKeyDown={onSubmit} /> :
+              <input className={styles.commentEdit} ref={editRef} value={content} onChange={onChange}
+                     onKeyDown={onSubmit}/> :
               <div className={styles.commentText}>{content}</div>
           }
           {isEdit || <div className={styles.reactButton}>답글</div>}
         </div>
       </div>
       <div className={styles.moreButton}>
-        <MoreButton setIsEdit={setIsEdit} />
+        <MoreButton setIsEdit={setIsEdit}/>
       </div>
     </div>
   )

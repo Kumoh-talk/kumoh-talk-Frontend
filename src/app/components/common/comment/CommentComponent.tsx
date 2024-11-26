dd363988855edaa99d0ae125107a18f9769bc348
oko_jin <dswvgw6412@naver.com>
2024-11-26 14:08:47 +0900

    refactor: 댓글 정보에 필요한 Props 제거
    Context로 가져오면 되기 때문에

--- src/app/components/common/comment/CommentComponent.tsx
+++ src/app/components/common/comment/CommentComponent.tsx
@@ -1,30 +1,21 @@
-"use client"
-
 import ProfileImage from '@/app/components/common/ProfileImage';
 import styles from './comment.module.scss';
 import CommentInput from '@/app/components/common/comment/CommentInput';
-import Comment from '@/app/components/common/comment/Comment'
-import useInput from '@/app/lib/hooks/useInput';
-import { KeyboardEvent } from 'react';
+import { CommentListProvider } from '@/app/components/common/comment/CommentListProvider';
+import ShowAccountComment from '@/app/components/common/comment/ShowAccountComment';
+import CommentList from '@/app/components/common/comment/CommentList';
 
 export default function CommentComponent() {
-  const [content, onChange, reset] = useInput({ comment: '' });
-
-  const onSubmit = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      console.log(content);
-      reset();
-    }
-  }
-
   return (
     <div className={styles.block}>
-      <div className={styles.head}>댓글 1</div>
-      <div className={styles.inputBlock}>
-        <ProfileImage />
-        <CommentInput placeholder={'댓글 추가'} name={'comment'} value={content.comment} onChange={onChange} onKeyDown={onSubmit} />
-      </div>
-      <Comment id={1} name={'황용진'} date={'2024.08.24'} comment={'테스트 댓글이요'} />
+      <CommentListProvider>
+        <ShowAccountComment/>
+        <div className={styles.inputBlock}>
+          <ProfileImage/>
+          <CommentInput/>
+        </div>
+        <CommentList/>
+      </CommentListProvider>
     </div>
   )
 }

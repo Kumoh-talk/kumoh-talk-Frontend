7b3351a68e234c08bd49abaad82251992ce89478
OhSeChan <seb_s19@naver.com>
2024-12-07 22:24:37 +0900

    feat: FormProvider를 통해 폼 컨텍스트 제공

--- src/app/components/apply/SeminarApplyForm/SeminarApplyForm.tsx
+++ src/app/components/apply/SeminarApplyForm/SeminarApplyForm.tsx
@@ -1,17 +1,25 @@
 'use client';
 
 import { useFormState } from 'react-dom';
+import { FormProvider } from 'react-hook-form';
 import submitAction from '@/app/lib/apis/apply/submitAction';
+import { useFormAction } from '@/app/lib/hooks/apply/useFormAction';
 import FormFields from '../FormFields/FormFields';
+import type { SeminarFormValues } from '@/app/lib/schemas/seminarFormSchema';
 import styles from './SeminarApplyForm.module.scss';
 
 const SeminarApplyForm = () => {
   const [formState, formAction] = useFormState(submitAction, null);
+  const form = useFormAction<SeminarFormValues>({
+    formState,
+  });
 
   return (
-    <form className={styles.seminarApplyForm} action={formAction}>
-      <FormFields />
-    </form>
+    <FormProvider {...form}>
+      <form className={styles.seminarApplyForm} action={formAction}>
+        <FormFields />
+      </form>
+    </FormProvider>
   );
 };
 

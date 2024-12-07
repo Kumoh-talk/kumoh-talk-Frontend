a282c61a3ee7a10f7431586cd085a8eaf522c764
OhSeChan <seb_s19@naver.com>
2024-12-07 22:16:04 +0900

    feat: 폼의 상태 관리를 위한 useFormAction 훅 구현

---
+++ src/app/lib/hooks/apply/useFormAction.ts
@@ -0,0 +1,48 @@
+'use client';
+
+import { useEffect } from 'react';
+import { FormState } from '@/app/lib/types/apply/formState';
+import { FieldValues, useForm, UseFormProps } from 'react-hook-form';
+
+type UseFormActionProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TContext = any
+> = UseFormProps<TFieldValues, TContext> & {
+  formState: FormState | null;
+};
+
+export function useFormAction<
+  TFieldValues extends FieldValues = FieldValues,
+  TContext = any
+>({ formState, ...props }: UseFormActionProps<TFieldValues, TContext>) {
+  const form = useForm({ ...props });
+
+  useEffect(() => {
+    if (!formState || !hasState(formState)) return;
+
+    form.clearErrors();
+
+    switch (formState.code) {
+      case 'VALIDATION_ERROR':
+        const { fieldErrors } = formState;
+
+        Object.keys(fieldErrors).forEach((key) => {
+          form.setError(key as any, {
+            message: fieldErrors[key].flat().join(' '),
+          });
+        });
+        break;
+      case 'SUCCESS':
+        form.reset();
+        break;
+    }
+  }, [formState, form]);
+
+  return { ...form };
+}
+
+const hasState = (formState: FormState | null): formState is FormState => {
+  if (!formState || typeof formState !== 'object') return false;
+
+  return 'code' in formState;
+};

69e76933382c7e5d87f3785ba1cf36d6a5cd9c4e
OhSeChan <seb_s19@naver.com>
2024-12-07 23:09:07 +0900

    feat: 폼 컨텍스트를 받아 InputField에 해당하는 필드 적용

--- src/app/components/apply/FormFields/FormFields.tsx
+++ src/app/components/apply/FormFields/FormFields.tsx
@@ -1,15 +1,17 @@
 'use client';
 
+import { useFormContext } from 'react-hook-form';
 import Label from '../Label/Label';
-import Input from '../Input/Input';
 import Select from '../Select/Select';
 import Checkbox from '../Checkbox/Checkbox';
 import Button from '@/app/components/common/button/Button';
+import InputField from '../Field/InputField';
 import { departments, grades } from '@/app/lib/constants/apply/selectValues';
 import { getUpcomingWeekdayDates } from '@/app/lib/utils/dateUtils';
 import styles from './FormFields.module.scss';
 
 const FormFields = () => {
+  const form = useFormContext();
   const dates = getUpcomingWeekdayDates(4, 3).map((date) => ({
     value: date,
     label: `${date} (목)`,
@@ -32,26 +34,27 @@ const FormFields = () => {
             <Select options={dates} />
           </div>
         </div>
-        <div className={styles.formField}>
-          <Label>학번</Label>
-          <Input />
-        </div>
-        <div className={styles.formField}>
-          <Label>전화번호</Label>
-          <Input />
-        </div>
-        <div className={styles.formField}>
-          <Label>발표하고 싶은 주제</Label>
-          <Input />
-        </div>
-        <div className={styles.formField}>
-          <Label>발표 주제의 이름</Label>
-          <Input />
-        </div>
-        <div className={styles.formField}>
-          <Label>예상 소요 시간</Label>
-          <Input />
-        </div>
+        <InputField control={form.control} name='studentId' label='학번' />
+        <InputField
+          control={form.control}
+          name='phoneNumber'
+          label='전화번호'
+        />
+        <InputField
+          control={form.control}
+          name='presentationTopic'
+          label='발표하고 싶은 주제'
+        />
+        <InputField
+          control={form.control}
+          name='seminarName'
+          label='발표 주제의 이름'
+        />
+        <InputField
+          control={form.control}
+          name='estimatedDuration'
+          label='예상 소요 시간'
+        />
         <div className={styles.row}>
           <Checkbox>야밤의 금오톡 게시글 작성 여부</Checkbox>
           <Checkbox>

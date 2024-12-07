3ad556c57067047c6e2a4e6f5866c4ad3fd543c2
OhSeChan <seb_s19@naver.com>
2024-12-07 21:57:20 +0900

    feat: 서버 액션 함수 사용을 위한 useFormState 추가

--- src/app/components/apply/SeminarApplyForm/SeminarApplyForm.tsx
+++ src/app/components/apply/SeminarApplyForm/SeminarApplyForm.tsx
@@ -1,11 +1,15 @@
 'use client';
 
+import { useFormState } from 'react-dom';
+import submitAction from '@/app/lib/apis/apply/submitAction';
 import FormFields from '../FormFields/FormFields';
 import styles from './SeminarApplyForm.module.scss';
 
 const SeminarApplyForm = () => {
+  const [formState, formAction] = useFormState(submitAction, null);
+
   return (
-    <form className={styles.seminarApplyForm}>
+    <form className={styles.seminarApplyForm} action={formAction}>
       <FormFields />
     </form>
   );

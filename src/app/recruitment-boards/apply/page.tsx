d90d4ce9b48cbe148df0d7d28d50c74b1cb731ff
oko_jin <dswvgw6412@naver.com>
2024-12-08 19:01:59 +0900

    feat: 스터디/프로젝트 게시물 상세 페이지 헤더랑 푸터 연결

--- src/app/recruitment-boards/apply/page.tsx
+++ src/app/recruitment-boards/apply/page.tsx
@@ -5,15 +5,21 @@ import {
   RecruitmentBoardDetailProvider
 } from '@/app/components/recruitment-boards/detail/RecruitmentBoardDetailProvider';
 import RecruitmentApplyContainer from '@/app/components/recruitment-boards/apply/RecruitmentApplyContainer';
+import Header from '@/app/components/common/header/Header';
+import Footer from '@/app/components/common/footer/Footer';
 
 export default function Page() {
   return (
-    <main className={styles.block}>
-      <ApplyBanner/>
-      <RecruitmentBoardDetailProvider>
-        <RecruitmentApplyContainer/>
-      </RecruitmentBoardDetailProvider>
-      <ApplyForm/>
-    </main>
+    <>
+      <Header title={`신청`}/>
+      <main className={styles.block}>
+        <ApplyBanner/>
+        <RecruitmentBoardDetailProvider>
+          <RecruitmentApplyContainer/>
+        </RecruitmentBoardDetailProvider>
+        <ApplyForm/>
+      </main>
+      <Footer/>
+    </>
   )
 }
--- src/app/recruitment-boards/list/page.tsx
+++ src/app/recruitment-boards/list/page.tsx
@@ -3,13 +3,19 @@ import {
   RecruitmentBoardDetailProvider
 } from '@/app/components/recruitment-boards/detail/RecruitmentBoardDetailProvider';
 import ApplyListContainer from '@/app/components/recruitment-boards/list/ApplyListContainer';
+import Header from '@/app/components/common/header/Header';
+import Footer from '@/app/components/common/footer/Footer';
 
 export default function Page() {
   return (
-    <main className={styles.block}>
-      <RecruitmentBoardDetailProvider>
-        <ApplyListContainer/>
-      </RecruitmentBoardDetailProvider>
-    </main>
+    <>
+      <Header title={`신청서 확인`}/>
+      <main className={styles.block}>
+        <RecruitmentBoardDetailProvider>
+          <ApplyListContainer/>
+        </RecruitmentBoardDetailProvider>
+      </main>
+      <Footer/>
+    </>
   )
 }

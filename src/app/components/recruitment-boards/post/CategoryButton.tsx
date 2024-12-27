8e882e90b3e26874a7c043ff4c46f18dc8b08d04
oko_jin <dswvgw6412@naver.com>
2024-12-27 21:39:50 +0900

    feat: 카테고리 버튼 구현

---
+++ src/app/components/recruitment-boards/post/CategoryButton.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import clsx from 'clsx';
+import styles from './categoryButton.module.scss';
+import { textToType } from '@/app/lib/constants/tag/tagValues';
+import { useContext } from 'react';
+import { TabsContext } from './TabsProvider';
+
+export interface Props {
+  content: string;
+  type: 'type' | 'tag';
+}
+
+export default function CategoryButton({ content, type }: Props) {
+  const { state, setState } = useContext(TabsContext);
+  const value = textToType[content].toLowerCase();
+  const isSelected = state.type === value || state.tag === value;
+
+  const onClick = () => {
+    setState({
+      ...state,
+      [type]: value,
+    });
+  };
+
+  return (
+    <button
+      className={clsx(styles.button, isSelected && styles[value])}
+      onClick={onClick}
+    >
+      {content}
+    </button>
+  );
+}

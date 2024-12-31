5ce17d943ef6a79cc0afa82528bdc51943cacd5d
oko_jin <dswvgw6412@naver.com>
2024-12-31 22:36:53 +0900

    refactor: 레이아웃으로 적용되어 있는 UI page.tsx로 이동

--- src/app/recruitment-boards/page.tsx
+++ src/app/recruitment-boards/page.tsx
@@ -1,9 +1,21 @@
 import ArticleList from '../components/articles/ArticleList';
+import Footer from '../components/common/footer/Footer';
+import Header from '../components/common/header/Header';
+import styles from './page.module.scss';
 
 export default function Home({
   searchParams,
 }: {
   searchParams: { category?: string; page?: number; order?: string };
 }) {
-  return <ArticleList searchParams={searchParams} />;
+  return (
+    <>
+      <Header title='게시글' />
+      <main className={styles.main}>
+        <header>게시글</header>
+        <ArticleList searchParams={searchParams} />
+      </main>
+      <Footer />
+    </>
+  );
 }

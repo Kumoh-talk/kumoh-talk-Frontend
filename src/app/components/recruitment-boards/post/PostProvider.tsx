55317573d12b118484f6f8f30e5e86d31b1f1737
oko_jin <dswvgw6412@naver.com>
2025-01-07 19:19:31 +0900

    feat: 신청폼을 관리하는 Provider 구현

---
+++ src/app/components/recruitment-boards/post/PostProvider.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { PostForm } from '@/app/lib/types/recruitmentBoards/post/postForm';
+import { createContext, useState } from 'react';
+
+export const PostContext = createContext<{
+  form: PostForm[];
+  setForm: (state: PostForm[]) => void;
+}>({
+  form: [
+    {
+      number: 1,
+      question: '',
+      type: 'description',
+      isEssential: true,
+      answerList: [],
+    },
+  ],
+  setForm: () => {},
+});
+
+export interface Props {
+  children: React.ReactNode;
+}
+
+export function PostProvider({ children }: Props) {
+  const [form, setForm] = useState<PostForm[]>([
+    {
+      number: 1,
+      question: '',
+      type: 'description',
+      isEssential: true,
+      answerList: [],
+    },
+  ]);
+
+  return (
+    <PostContext.Provider value={{ form, setForm }}>
+      {children}
+    </PostContext.Provider>
+  );
+}

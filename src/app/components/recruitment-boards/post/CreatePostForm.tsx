561fdd78f7c09e2315952f9863c9813b872e613c
oko_jin <dswvgw6412@naver.com>
2025-01-07 19:56:46 +0900

    feat: PostContext로 동작하도록 구현

--- src/app/components/recruitment-boards/post/CreatePostForm.tsx
+++ src/app/components/recruitment-boards/post/CreatePostForm.tsx
@@ -1,25 +1,18 @@
 'use client';
 
-import { useRef, useState } from 'react';
+import { useContext, useRef } from 'react';
 import { Plus } from 'lucide-react';
 import styles from './createPostForm.module.scss';
-import QuestionInputField, { QuestionType } from './QuestionInputField';
+import QuestionInputField from './QuestionInputField';
+import { PostContext } from './PostProvider';
 
 export default function CreatePostForm() {
-  const [questionArr, setQuestionArr] = useState<QuestionType[]>([
-    {
-      number: 1,
-      question: '',
-      type: 'description',
-      isEssential: false,
-      answerList: [],
-    },
-  ]);
+  const { form, setForm } = useContext(PostContext);
   const questionRef = useRef(1);
 
   const addQuestion = () => {
-    setQuestionArr([
-      ...questionArr,
+    setForm([
+      ...form,
       {
         number: ++questionRef.current,
         question: '',
@@ -31,18 +24,16 @@ export default function CreatePostForm() {
   };
 
   const deleteQuestion = (number: number) => {
-    setQuestionArr(
-      questionArr.filter((question) => question.number !== number)
-    );
+    setForm(form.filter((question) => question.number !== number));
   };
 
   return (
     <div className={styles.createPostForm}>
-      {questionArr.map((question) => (
+      {form.map((question) => (
         <QuestionInputField
           question={question}
-          questionArr={questionArr}
-          setQuestionArr={setQuestionArr}
+          questionArr={form}
+          setQuestionArr={setForm}
           deleteQuestion={deleteQuestion}
           key={question.number}
         />

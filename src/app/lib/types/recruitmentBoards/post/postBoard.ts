f323d5ce06da9e331dac9e75e9ebd54713d329a5
oko_jin <dswvgw6412@naver.com>
2025-01-07 19:19:49 +0900

    feat: 모집글의 board 타입 구현

---
+++ src/app/lib/types/recruitmentBoards/post/postBoard.ts
@@ -0,0 +1,13 @@
+export interface PostBoard {
+  title: string;
+  summary: string;
+  host: string;
+  content: string;
+  recruitmentTarget: string;
+  recruitmentNum: number;
+  currentMemberNum: number;
+  recruitmentDeadline: string;
+  activityStart: string;
+  activityFinish: string;
+  activityCycle: string;
+}

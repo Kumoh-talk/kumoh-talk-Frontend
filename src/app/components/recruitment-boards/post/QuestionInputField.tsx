42a89acbe0a4a02ca8247f7fc77357f35e53e488
oko_jin <dswvgw6412@naver.com>
2025-01-07 20:31:06 +0900

    feat: selectType 변경, 필수 토글 기능 구현

--- src/app/components/recruitment-boards/post/QuestionInputField.tsx
+++ src/app/components/recruitment-boards/post/QuestionInputField.tsx
@@ -1,25 +1,45 @@
 'use client';
 
-import { PostForm } from '@/app/lib/types/recruitmentBoards/post/postForm';
+import {
+  selectType,
+  PostForm,
+} from '@/app/lib/types/recruitmentBoards/post/postForm';
 import Select from '../../apply/Select/Select';
 import Switch from '../../common/Switch/Switch';
 import styles from './questionInputField.module.scss';
 import { Trash } from 'lucide-react';
 import { formOptions } from '@/app/lib/constants/recruitmentBoards/post/formOptions';
+import { useContext } from 'react';
+import { PostContext } from './PostProvider';
 
 export interface Props {
   question: PostForm;
-  questionArr: PostForm[];
-  setQuestionArr: (questionArr: PostForm[]) => void;
   deleteQuestion: (number: number) => void;
 }
 
 export default function QuestionInputField({
   question,
-  questionArr,
-  setQuestionArr,
   deleteQuestion,
 }: Props) {
+  const { form, setForm } = useContext(PostContext);
+
+  const onSelectChange = (value: string) => {
+    const selectType = value as selectType;
+    const newQuestionArr = form.map((q) =>
+      q.number === question.number ? { ...q, type: selectType } : q
+    );
+    setForm([...newQuestionArr]);
+  };
+
+  const essentialChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newQuestionArr = form.map((q) =>
+      q.number === question.number
+        ? { ...q, isEssential: event.target.checked }
+        : q
+    );
+    setForm(newQuestionArr);
+  };
+
   return (
     <div className={styles.questionInputField}>
       <div className={styles.leftWall} />
@@ -29,17 +49,17 @@ export default function QuestionInputField({
             className={styles.input}
             value={question.question}
             onChange={(e) => {
-              const newQuestionArr = questionArr.map((q) =>
+              const newQuestionArr = form.map((q) =>
                 q.number === question.number
                   ? { ...q, question: e.target.value }
                   : q
               );
-              setQuestionArr(newQuestionArr);
+              setForm(newQuestionArr);
             }}
             placeholder='질문 제목을 입력하세요'
           />
           <div className={styles.select}>
-            <Select options={formOptions} onChange={() => {}} />
+            <Select options={formOptions} onChange={onSelectChange} />
           </div>
         </div>
         <div className={styles.questionBottom}>
@@ -51,7 +71,7 @@ export default function QuestionInputField({
           </div>
           <div className={styles.requiredSwitch}>
             <p>필수</p>
-            <Switch defaultChecked />
+            <Switch checked={question.isEssential} onChange={essentialChange} />
           </div>
         </div>
       </div>

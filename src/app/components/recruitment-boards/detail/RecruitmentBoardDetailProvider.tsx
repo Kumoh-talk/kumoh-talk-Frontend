b2736daccee0c9b25bfcdb02601428c156e22cb2
oko_jin <dswvgw6412@naver.com>
2025-01-22 14:24:54 +0900

    chore: form을 rhf 방식으로 변경

--- src/app/components/recruitment-boards/detail/RecruitmentBoardDetailProvider.tsx
+++ src/app/components/recruitment-boards/detail/RecruitmentBoardDetailProvider.tsx
@@ -1,7 +1,10 @@
 'use client';
 
 import { createContext, useState, useEffect } from 'react';
-import { RecruitmentBoards,  RecruitmentBoardsApi } from '@/app/lib/types/recruitmentBoards/recruitmentBoards';
+import {
+  RecruitmentBoards,
+  RecruitmentBoardsApi,
+} from '@/app/lib/types/recruitmentBoards/recruitmentBoards';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 export const RecruitmentBoardDetailContext = createContext<{
@@ -28,7 +31,9 @@ export function RecruitmentBoardDetailProvider({ children }: Props) {
   const fetchData = async () => {
     const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
     try {
-      const response = await fetch(`${API_URL}/api/v1/recruitment-boards/${boardId}/board`);
+      const response = await fetch(
+        `${API_URL}/api/v1/recruitment-boards/${boardId}/board`
+      );
       const result = await response.json();
 
       if (response.ok && 'success' in result && 'data' in result) {
@@ -48,7 +53,9 @@ export function RecruitmentBoardDetailProvider({ children }: Props) {
   }, []);
 
   return (
-    <RecruitmentBoardDetailContext.Provider value={{ success: state.success, data: state.data, fetchData }}>
+    <RecruitmentBoardDetailContext.Provider
+      value={{ success: state.success, data: state.data, fetchData }}
+    >
       {children}
     </RecruitmentBoardDetailContext.Provider>
   );
--- src/app/recruitment-boards/apply/page.tsx
+++ src/app/recruitment-boards/apply/page.tsx
@@ -2,6 +2,7 @@ import ApplyBanner from '@/app/components/recruitment-boards/apply/ApplyBanner';
 import styles from './page.module.scss';
 import ApplyForm from '@/app/components/recruitment-boards/apply/ApplyForm';
 import { RecruitmentBoardDetailProvider } from '@/app/components/recruitment-boards/detail/RecruitmentBoardDetailProvider';
+import RecruitmentApplyForm from '@/app/components/recruitment-boards/apply/RecruitmentApplyForm';
 import RecruitmentApplyContainer from '@/app/components/recruitment-boards/apply/RecruitmentApplyContainer';
 import Header from '@/app/components/common/header/Header';
 import Footer from '@/app/components/common/footer/Footer';
@@ -23,6 +24,7 @@ export default function Page({
       <Header title={`${title} 신청`} />
       <main className={styles.block}>
         <ApplyBanner />
+        <RecruitmentApplyForm />
         <RecruitmentBoardDetailProvider>
           <RecruitmentApplyContainer />
         </RecruitmentBoardDetailProvider>

a9b2fcdc85a2fff6b5f1be7cf01866c3ed8909ba
oko_jin <dswvgw6412@naver.com>
2025-01-29 18:52:49 +0900

    feat: 답글 대상을 보여주는 텍스트 추가

--- src/app/components/common/comment/Comment.tsx
+++ src/app/components/common/comment/Comment.tsx
@@ -1,56 +1,95 @@
-'use client'
+'use client';
 
 import ProfileImage from '@/app/components/common/ProfileImage';
-import styles from './comment.module.scss'
+import styles from './comment.module.scss';
 import MoreButton from '@/app/components/common/comment/MoreButton';
 import { useEffect, useRef, useState } from 'react';
 import useCommentEdit from '@/app/lib/hooks/useCommentEdit';
 import dayjs from 'dayjs';
+import { CommentInfoResponseList } from '@/app/lib/types/comment/commentList';
 
 export interface Props {
-  id: number;
+  commentId: number;
+  parentCommentName?: string | null;
   name: string;
   date: string;
   comment: string;
+  replyComments: CommentInfoResponseList[];
 }
 
-export default function Comment({ id, name, date, comment }: Props) {
+export default function Comment({
+  commentId,
+  parentCommentName = null,
+  name,
+  date,
+  comment,
+  replyComments,
+}: Props) {
   const [isEdit, setIsEdit] = useState(false);
-  const [content, onChange, onSubmit] = useCommentEdit({ id, comment, setIsEdit });
+  const [content, onChange, onSubmit] = useCommentEdit({
+    id: commentId,
+    currentComment: comment,
+    setIsEdit,
+  });
   const editRef: any = useRef(null);
 
   useEffect(() => {
     if (isEdit) {
       editRef.current?.focus();
     }
-  }, [isEdit])
+  }, [isEdit]);
 
   return (
-    <div className={styles.commentBlock}>
-      <ProfileImage/>
-      <div className={styles.commentMain}>
-        <div className={styles.commentTop}>
-          <div className={styles.commentName}>{name}</div>
-          <div className={styles.commentDate}>{dayjs(date).format('YYYY.MM.DD')}</div>
-        </div>
-        <div className={styles.commentBottom}>
-          {
-            isEdit ?
-              <textarea
-                className={styles.commentEdit}
-                ref={editRef}
-                value={content}
-                onChange={onChange}
-                onKeyDown={onSubmit}
-              /> :
-              <div className={styles.commentText}>{content}</div>
-          }
-          {isEdit || <div className={styles.reactButton}>답글</div>}
+    <>
+      <div className={styles.commentWrapper}>
+        {parentCommentName && <div className={styles.leftPadding} />}
+        <div className={styles.commentBlock}>
+          <ProfileImage />
+          <div className={styles.commentMain}>
+            <div className={styles.commentTop}>
+              <div className={styles.commentName}>{name}</div>
+              <div className={styles.commentDate}>
+                {dayjs(date).format('YYYY.MM.DD')}
+              </div>
+            </div>
+            <div className={styles.commentBottom}>
+              {isEdit ? (
+                <textarea
+                  className={styles.commentEdit}
+                  ref={editRef}
+                  value={content}
+                  onChange={onChange}
+                  onKeyDown={onSubmit}
+                />
+              ) : (
+                <span className={styles.commentText}>
+                  {parentCommentName || (
+                    <span className={styles.reply}>
+                      @ &nbsp; {parentCommentName}
+                    </span>
+                  )}
+                  {content}
+                </span>
+              )}
+              {isEdit || <div className={styles.reactButton}>답글</div>}
+            </div>
+          </div>
+          <div className={styles.moreButton}>
+            <MoreButton commentId={commentId} setIsEdit={setIsEdit} />
+          </div>
         </div>
       </div>
-      <div className={styles.moreButton}>
-        <MoreButton setIsEdit={setIsEdit}/>
-      </div>
-    </div>
-  )
+      {replyComments.map((replyComment) => (
+        <Comment
+          commentId={replyComment.commentId}
+          key={replyComment.commentId}
+          parentCommentName={name}
+          name={replyComment.userNickname}
+          date={replyComment.createdAt}
+          comment={replyComment.content}
+          replyComments={replyComment.replyComments}
+        />
+      ))}
+    </>
+  );
 }
--- src/app/components/common/comment/CommentList.tsx
+++ src/app/components/common/comment/CommentList.tsx
@@ -11,11 +11,12 @@ export default function CommentList({ commentList }: Props) {
     <div className={styles.commentList}>
       {commentList.commentInfoResponseList.map((comment) => (
         <Comment
-          id={comment.commentId}
+          commentId={comment.commentId}
           key={comment.commentId}
           name={comment.userNickname}
           date={comment.createdAt}
           comment={comment.content}
+          replyComments={comment.replyComments}
         />
       ))}
     </div>

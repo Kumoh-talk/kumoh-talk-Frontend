93c23b92510234a0894852c10aea0237018288e0
oko_jin <dswvgw6412@naver.com>
2025-01-29 21:41:32 +0900

    refactor: 댓글 더보기 탭 클릭시 사용하는 기능들 리팩토링

---
+++ src/app/lib/hooks/useCommentMoreBubble.ts
@@ -0,0 +1,48 @@
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
+import { deleteRecruitmentBoardComment } from '../apis/recruitment-boards/recruitmentBoard';
+
+export default function useCommentMoreBubble(
+  commentId: number,
+  setIsEdit: Dispatch<SetStateAction<boolean>>
+) {
+  const [isOpen, setIsOpen] = useState(false);
+  const bubbleRef = useRef<HTMLDivElement>(null);
+
+  const onModify = () => {
+    setIsEdit(true);
+    setIsOpen(false);
+  };
+
+  const onDelete = async () => {
+    setIsOpen(false);
+    await deleteRecruitmentBoardComment(commentId);
+    window.location.reload();
+  };
+
+  const onInsideClick = (event: React.MouseEvent) => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const onClickOutside = (event: MouseEvent) => {
+      if (
+        bubbleRef.current &&
+        !bubbleRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      window.addEventListener('click', onClickOutside);
+    } else {
+      window.removeEventListener('click', onClickOutside);
+    }
+
+    return () => {
+      window.removeEventListener('click', onClickOutside);
+    };
+  }, [isOpen]);
+
+  return { bubbleRef, isOpen, onModify, onDelete, onInsideClick };
+}

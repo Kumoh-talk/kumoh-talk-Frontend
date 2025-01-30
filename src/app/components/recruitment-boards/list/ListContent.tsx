a8868f9e7a8e3c8e99b66ac8f962cfe3b618d49d
oko_jin <dswvgw6412@naver.com>
2025-01-30 22:22:16 +0900

    feat: 조회한 데이터를 기반으로 UI 구현

--- src/app/components/recruitment-boards/list/ListContent.tsx
+++ src/app/components/recruitment-boards/list/ListContent.tsx
@@ -1,24 +1,57 @@
 import ProfileImage from '@/app/components/common/ProfileImage';
 import styles from './listContent.module.scss';
 import Link from 'next/link';
+import { Applicant } from '@/app/lib/types/recruitmentBoards/applyList';
+import { getApplicantUserInfo } from '@/app/lib/apis/recruitment-boards/list/list';
 
-export default function ListContent() {
+type UserAdditionalProfile = {
+  studentStatus: string;
+  department: string;
+  grade: number;
+  studentId: number;
+};
+
+type ApplicantUserInfo = {
+  userId: number;
+  name: string;
+  profileImageUrl: string;
+  userAdditionalProfile: UserAdditionalProfile;
+  createdAt: string;
+  updatedAt: string;
+};
+
+type ApplicantUserInfoApi = {
+  success: string;
+  data: ApplicantUserInfo;
+};
+
+export default async function ListContent({
+  applicant,
+}: {
+  applicant: Applicant;
+}) {
+  const { applicantId, userId, createdAt, updatedAt } = applicant;
+  const applicationUserInfo: ApplicantUserInfoApi = await getApplicantUserInfo(
+    userId
+  );
+  const { name, userAdditionalProfile } = applicationUserInfo.data;
+
   return (
     <Link
       style={{ textDecoration: 'none' }}
-      href={`./list/${'applicantId'}?id=${'게시물ID'}&boardType=${'게시물 타입'}`}
+      href={`./list/${applicantId}?id=${'게시물ID'}&boardType=${'게시물 타입'}`}
       passHref={true}
     >
       <div className={styles.contentBlock}>
         <div className={styles.profile}>
           <ProfileImage />
-          <p className={styles.name}>황용진</p>
+          <p className={styles.name}>{name}</p>
         </div>
-        <p>컴퓨터공학과</p>
-        <p>3학년</p>
+        <p>{userAdditionalProfile.department}</p>
+        <p>{userAdditionalProfile.grade}</p>
         <p>20201287</p>
         <p>010 1234 5678</p>
-        <p>재학중</p>
+        <p>{userAdditionalProfile.studentStatus}</p>
       </div>
     </Link>
   );

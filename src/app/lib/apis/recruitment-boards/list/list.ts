2c965df392db1fabf77ef6f25c92a66ebebe645f
oko_jin <dswvgw6412@naver.com>
2025-01-30 22:24:12 +0900

    feat: 신청자 리스트와 상세 정보 조회 api 구현

---
+++ src/app/lib/apis/recruitment-boards/list/list.ts
@@ -0,0 +1,50 @@
+const baseUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/v1`;
+
+const _fetch = async (
+  url: string,
+  options: RequestInit,
+  body?: string | any
+) => {
+  if (body && typeof body !== 'string') {
+    options.body = JSON.stringify(body);
+  }
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      throw new Error(JSON.stringify(await response.json()));
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+export const getRecruitmentBoardApplicantList = (
+  recruitmentBoardId: string,
+  page: number,
+  sort: string
+) => {
+  return _fetch(
+    `${baseUrl}/applications/recruitment/${recruitmentBoardId}?page=${page}&size=${10}&sort=${sort}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    }
+  );
+};
+
+export const getApplicantUserInfo = (userId: number) => {
+  return _fetch(`${baseUrl}/users/${userId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  });
+};

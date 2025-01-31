eae67d4a01186c53bfb33c801a192eece9e98b90
oko_jin <dswvgw6412@naver.com>
2025-01-31 22:49:37 +0900

    feat: props 구성 변경

--- src/app/recruitment-boards/list/page.tsx
+++ src/app/recruitment-boards/list/page.tsx
@@ -1,21 +1,42 @@
 import styles from './page.module.scss';
-import {
-  RecruitmentBoardDetailProvider
-} from '@/app/components/recruitment-boards/detail/RecruitmentBoardDetailProvider';
 import ApplyListContainer from '@/app/components/recruitment-boards/list/ApplyListContainer';
 import Header from '@/app/components/common/header/Header';
 import Footer from '@/app/components/common/footer/Footer';
+import { getRecruitmentBoardApplicantList } from '@/app/lib/apis/recruitment-boards/list/list';
+import { ApplyListApi } from '@/app/lib/types/recruitmentBoards/applyList';
 
-export default function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: {
+    id: string;
+    title: string;
+    boardType: string;
+    tag: string;
+    page: string;
+    sort: string;
+  };
+}) {
+  const { id, title, boardType, tag, page, sort } = searchParams;
+  const applicantList: ApplyListApi = await getRecruitmentBoardApplicantList(
+    id,
+    Number(page),
+    sort
+  );
+
   return (
     <>
-      <Header title={`신청서 확인`}/>
+      <Header title={`신청서 확인`} />
       <main className={styles.block}>
-        <RecruitmentBoardDetailProvider>
-          <ApplyListContainer/>
-        </RecruitmentBoardDetailProvider>
+        <ApplyListContainer
+          id={id}
+          title={title}
+          boardType={boardType}
+          tag={tag}
+          applicantList={applicantList}
+        />
       </main>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }

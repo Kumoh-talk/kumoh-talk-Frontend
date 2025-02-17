f79524a368e5d0bf4e1534c36ea2124f95927b8e
oko_jin <dswvgw6412@naver.com>
2025-02-17 22:34:57 +0900

    fix: 추가 정보 상태변경

--- src/app/components/profile/ProfileContent.tsx
+++ src/app/components/profile/ProfileContent.tsx
@@ -39,7 +39,7 @@ export default function ProfileContent() {
         if (profileData?.data) {
           setMyProfile(profileData.data);
           const additionalData = await getAdditionalInfo();
-          setAdditionalInfo(additionalData);
+          setAdditionalInfo(additionalData.data);
         }
       } catch (error) {
         console.error('Error fetching profile:', error);

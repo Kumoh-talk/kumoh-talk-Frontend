48c7479ad3e1a4cc8daf145e44e82dc94629f844
oko_jin <dswvgw6412@naver.com>
2025-02-17 22:36:55 +0900

    feat: 신청폼 필드 초기 설정

--- src/app/components/recruitment-boards/apply/RecruitmentApplyForm.tsx
+++ src/app/components/recruitment-boards/apply/RecruitmentApplyForm.tsx
@@ -2,51 +2,25 @@
 
 import { FormProvider, useForm } from 'react-hook-form';
 import RecruitmentApplyFormField from './RecruitmentApplyFormField';
-import { PostBoard } from '@/app/lib/types/recruitmentBoards/post/postBoard';
-import { useTransition } from 'react';
-
-const defaultValues: PostBoard = {
-  title: '',
-  summary: '',
-  host: '',
-  content: '',
-  recruitmentTarget: '',
-  recruitmentNum: 0,
-  currentMemberNum: 0,
-  recruitmentDeadline: '',
-  activityStart: '',
-  activityFinish: '',
-  activityCycle: '',
-};
-
-async function getQuestions() {
-  const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
-  try {
-    const response = await fetch(`${API_URL}/api/v1/recruitment-boards/form`);
-    const result = await response.json();
-    if (response.ok && 'success' in result && 'data' in result) {
-      return result.data;
-    } else {
-      console.error('Failed to fetch data:', result);
-      return [];
-    }
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return [];
-  }
+import { Questions } from '@/app/lib/types/recruitmentBoards/apply/apply';
+export interface Props {
+  questions: Questions[];
 }
 
-export default function RecruitmentApplyForm() {
-  const formState = useForm({ defaultValues });
-  const [isPending, startTransition] = useTransition();
+export default function RecruitmentApplyForm({ questions }: Props) {
+  const defaultValues = Object.fromEntries(
+    questions.map(({ questionId }) => [questionId, ''])
+  );
 
-  const onSubmit = (data: PostBoard) => console.log(data);
+  const formState = useForm({ defaultValues });
+
+  const onSubmit = (data: { [key: string]: string }) => console.log(data);
   const onError = (error: any) => console.error(error);
 
   return (
     <FormProvider {...formState}>
       <form onSubmit={formState.handleSubmit(onSubmit, onError)} noValidate>
-        <RecruitmentApplyFormField />
+        <RecruitmentApplyFormField questions={questions} />
       </form>
     </FormProvider>
   );
--- src/app/recruitment-boards/apply/page.tsx
+++ src/app/recruitment-boards/apply/page.tsx
@@ -1,32 +1,40 @@
 import ApplyBanner from '@/app/components/recruitment-boards/apply/ApplyBanner';
 import styles from './page.module.scss';
-import ApplyForm from '@/app/components/recruitment-boards/apply/ApplyForm';
-import { RecruitmentBoardDetailProvider } from '@/app/components/recruitment-boards/detail/RecruitmentBoardDetailProvider';
 import RecruitmentApplyForm from '@/app/components/recruitment-boards/apply/RecruitmentApplyForm';
 import RecruitmentApplyContainer from '@/app/components/recruitment-boards/apply/RecruitmentApplyContainer';
 import Header from '@/app/components/common/header/Header';
 import Footer from '@/app/components/common/footer/Footer';
-import { matchRecruitmentTitle } from '@/app/lib/apis/recruitment-boards/boardDetail';
+import { matchRecruitmentTitle } from '@/app/lib/apis/recruitment-boards/recruitmentBoard';
+import { getQuestions } from '@/app/lib/apis/recruitment-boards/apply/apply';
+import { QuestionsResponse } from '@/app/lib/types/recruitmentBoards/apply/apply';
 
-export default function Page({
-  params,
+export default async function Page({
   searchParams,
 }: {
-  params: { id: string };
-  searchParams: { boardType: string };
+  searchParams: {
+    id: string;
+    title: string;
+    boardType: string;
+    tag: string;
+    detail: string;
+  };
 }) {
-  const title = matchRecruitmentTitle(searchParams.boardType);
+  const { id, title, boardType, tag, detail } = searchParams;
+  const headerTitle = matchRecruitmentTitle(searchParams.boardType);
+  const questions: QuestionsResponse = await getQuestions(id);
 
   return (
     <>
-      <Header title={`${title} 신청`} />
+      <Header title={`${headerTitle} 신청`} />
       <main className={styles.block}>
         <ApplyBanner />
-        <RecruitmentApplyForm />
-        <RecruitmentBoardDetailProvider>
-          <RecruitmentApplyContainer />
-        </RecruitmentBoardDetailProvider>
-        <ApplyForm />
+        <RecruitmentApplyContainer
+          title={title}
+          boardType={boardType}
+          tag={tag}
+          detail={detail}
+        />
+        <RecruitmentApplyForm questions={questions.data} />
       </main>
       <Footer />
     </>

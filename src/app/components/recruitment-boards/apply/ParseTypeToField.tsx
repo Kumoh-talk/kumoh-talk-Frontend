537ce75841f3dad532ff84a0201d407c8cf2c425
oko_jin <dswvgw6412@naver.com>
2025-02-18 23:47:20 +0900

    feat: 폼 타입에 따른 다른 입력폼을 반환하는 고차 컴포넌트 구현

---
+++ src/app/components/recruitment-boards/apply/ParseTypeToField.tsx
@@ -0,0 +1,52 @@
+import { Control } from 'react-hook-form';
+import InputField from '../../apply/Field/InputField';
+import CheckboxField from '../../apply/Field/CheckboxField';
+import ChoiceField from '../../apply/Field/ChoiceField';
+import { Questions } from '@/app/lib/types/recruitmentBoards/apply/apply';
+
+export default function ParseTypeToField({
+  control,
+  question,
+}: {
+  control: Control;
+  question: Questions;
+}) {
+  switch (question.type) {
+    case 'DESCRIPTION':
+      return (
+        <InputField
+          control={control}
+          name={`${question.questionId}`}
+          label={`${question.question}`}
+          placeholder={
+            question.isEssential
+              ? '필수 입력 칸 입니다.'
+              : '선택 입력 칸 입니다.'
+          }
+          required={question.isEssential}
+        />
+      );
+    case 'CHOICE':
+      return (
+        <ChoiceField
+          control={control}
+          name={`${question.questionId}`}
+          values={question.answerList}
+          label={`${question.question}`}
+          required={question.isEssential}
+        />
+      );
+    case 'CHECKBOX':
+      return (
+        <CheckboxField
+          control={control}
+          name={`${question.questionId}`}
+          values={question.answerList}
+          label={`${question.question}`}
+          required={question.isEssential}
+        />
+      );
+    default:
+      return <div>error</div>;
+  }
+}

b39cbae88579a040551575159d9bfc1a15b74358
oko_jin <dswvgw6412@naver.com>
2025-02-18 23:47:50 +0900

    feat: 신청폼 입력 페이지 UI 구현

--- src/app/components/recruitment-boards/apply/RecruitmentApplyFormField.tsx
+++ src/app/components/recruitment-boards/apply/RecruitmentApplyFormField.tsx
@@ -1,11 +1,16 @@
 'use client';
 
 import { useFormContext } from 'react-hook-form';
-import InputField from '../../apply/Field/InputField';
 import styles from './recruitmentApplyFormField.module.scss';
 import Button from '../../common/button/Button';
+import { Questions } from '@/app/lib/types/recruitmentBoards/apply/apply';
+import ParseTypeToField from './ParseTypeToField';
 
-export default function RecruitmentApplyFormField() {
+export interface Props {
+  questions: Questions[];
+}
+
+export default function RecruitmentApplyFormField({ questions }: Props) {
   const {
     register,
     handleSubmit,
@@ -16,23 +21,11 @@ export default function RecruitmentApplyFormField() {
   return (
     <>
       <div className={styles.field}>
-        <div className={styles.row}>
-          <InputField control={control} name="name" label="이름" />
-          <InputField control={control} name="email" label="학과" />
-        </div>
-        <div className={styles.row}>
-          <InputField control={control} name="phone" label="학년" />
-          <InputField control={control} name="github" label="학번" />
-        </div>
-        <div className={styles.row}>
-          <InputField control={control} name="portfolio" label="재학상태" />
-        </div>
-        <div className={styles.row}>
-          <InputField control={control} name="phoneNumber" label="전화번호" />
-        </div>
-        <div className={styles.row}>
-          <InputField control={control} name="message" label="지원동기" />
-        </div>
+        {questions.map((question) => (
+          <div key={question.questionId} className={styles.row}>
+            <ParseTypeToField control={control} question={question} />
+          </div>
+        ))}
         <div className={styles.row}>
           <Button type="submit" size="large">
             지원하기

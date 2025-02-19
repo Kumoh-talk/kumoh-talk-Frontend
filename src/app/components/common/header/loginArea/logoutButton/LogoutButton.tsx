b60e02396005eebf9de85de363186412a4e34c2d
wei756 <wei756fg@gmail.com>
2025-02-19 22:52:48 +0900

    feat: 로그아웃 구현

--- src/app/components/common/header/loginArea/logoutButton/LogoutButton.tsx
+++ src/app/components/common/header/loginArea/logoutButton/LogoutButton.tsx
@@ -1,5 +1,24 @@
+'use client';
+
+import { logout } from '@/app/lib/apis/user';
 import styles from './logoutButton.module.scss';
 
 export default function LogoutButton() {
-  return <button className={styles.logout}>로그아웃</button>;
+  const onClick = async () => {
+    try {
+      const res = await (await logout()).json();
+      if (res.success == 'true') {
+        console.log('로그아웃 성공');
+      }
+      window.location.href = '/?logout=true';
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <button className={styles.logout} onClick={onClick}>
+      로그아웃
+    </button>
+  );
 }

2e9ff072dcdf4ebd2384a6eb2188ecec65874c67
wei756 <wei756fg@gmail.com>
2025-02-19 23:19:10 +0900

    feat: 마이페이지 링크 추가

--- src/app/components/common/header/loginArea/LoginArea.tsx
+++ src/app/components/common/header/loginArea/LoginArea.tsx
@@ -1,27 +1,23 @@
 import LoginButton from './loginButton/LoginButton';
 import LogoutButton from './logoutButton/LogoutButton';
 import styles from './loginArea.module.scss';
-import { getUserInfo } from '@/app/lib/apis/user';
 import { cookies } from 'next/headers';
 import WelcomeBubble from './welcomeBubble/WelcomeBubble';
 import { parseJwt } from '@/app/lib/apis/auth';
+import Link from 'next/link';
 
 export default async function LoginArea() {
   const accessToken = cookies().get('accessToken')?.value;
 
-  const offline = (
-    <div className={styles.loginArea}>
-      <LoginButton />
-    </div>
-  );
-
   // 로그아웃 상태
   if (!accessToken) {
-    return offline;
+    return (
+      <div className={styles.loginArea}>
+        <LoginButton />
+      </div>
+    );
   }
-  const userInfoResponse = await getUserInfo(cookies().toString());
-  const userInfo = await userInfoResponse.json();
-  
+
   // 기본 정보 입력 안 됐을 때
   if (parseJwt(accessToken).USER_ROLE === 'ROLE_GUEST') {
     return (
@@ -31,9 +27,13 @@ export default async function LoginArea() {
       </div>
     );
   }
+
   return (
-    <div className={styles.loginArea}>
-      <LogoutButton />
-    </div>
+    <>
+      <Link href="/profile">마이페이지</Link>
+      <div className={styles.loginArea}>
+        <LogoutButton />
+      </div>
+    </>
   );
 }

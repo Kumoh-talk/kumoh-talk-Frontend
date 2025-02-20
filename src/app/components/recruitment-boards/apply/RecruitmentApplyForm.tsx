77b4eeabd2fcca0ece9c8ad54acb4e2d07a8428f
oko_jin <dswvgw6412@naver.com>
2025-02-20 13:45:26 +0900

    feat: 필수 여부에 따른 예외처리 적용

--- src/app/components/recruitment-boards/apply/RecruitmentApplyForm.tsx
+++ src/app/components/recruitment-boards/apply/RecruitmentApplyForm.tsx
@@ -5,6 +5,8 @@ import RecruitmentApplyFormField from './RecruitmentApplyFormField';
 import { Questions } from '@/app/lib/types/recruitmentBoards/apply/apply';
 import { postApplication } from '@/app/lib/apis/recruitment-boards/apply/post';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { recruitmentApplySchema } from './../../../lib/schemas/recruitmentApplySchema';
 export interface Props {
   recruitmentBoardId: string;
   questions: Questions[];
@@ -32,12 +34,16 @@ export default function RecruitmentApplyForm({
   questions,
 }: Props) {
   const defaultValues = Object.fromEntries(
-    questions.map(({ questionId }) => [questionId, ''])
+    questions.map(({ questionId, type }) => [
+      questionId,
+      type === 'DESCRIPTION' ? '' : [],
+    ])
   );
   const router = useRouter();
-  const formState = useForm({ defaultValues });
+  const schema = recruitmentApplySchema(questions);
+  const formState = useForm({ defaultValues, resolver: zodResolver(schema) });
 
-  const onSubmit = async (data: { [key: string]: string }) => {
+  const onSubmit = async (data: { [key: string]: any }) => {
     const formData = parseFormDataToRequestBody(data);
     const response = await postApplication(recruitmentBoardId, formData);
     console.log(response);

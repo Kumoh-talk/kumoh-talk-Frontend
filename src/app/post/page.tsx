fbec2dcfd373028231b3cf180c14c6d90d50a462
OhSeChan <seb_s19@naver.com>
2025-02-28 16:55:01 +0900

    feat: 게시물 작성 페이지 기본 레이아웃 구현

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,9 +1,18 @@
 import Header from '../components/post/Header/Header';
+import styles from './page.module.scss';
 
 export default function Post() {
   return (
     <>
       <Header />
+      <div>에디터 메뉴</div>
+      <main className={styles.layout}>
+        <div className={styles.content}>
+          <input className={styles.title} placeholder='제목을 입력하세요' />
+          <div className={styles.editorContainer}>에디터</div>
+          <div>해시태그</div>
+        </div>
+      </main>
     </>
   );
 }

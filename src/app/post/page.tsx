3df025a26c38b9d687f4076d0b65b62713f4df24
OhSeChan <seb_s19@naver.com>
2025-02-28 17:15:41 +0900

    feat: 에디터 기본 구조 구현

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
 import Header from '../components/post/Header/Header';
 import styles from './page.module.scss';
 
 export default function Post() {
+  const editor = useEditor({
+    extensions: [StarterKit],
+  });
+
   return (
     <>
       <Header />
@@ -9,7 +17,9 @@ export default function Post() {
       <main className={styles.layout}>
         <div className={styles.content}>
           <input className={styles.title} placeholder='제목을 입력하세요' />
-          <div className={styles.editorContainer}>에디터</div>
+          <div className={styles.editorContainer}>
+            <EditorContent editor={editor} />
+          </div>
           <div>해시태그</div>
         </div>
       </main>

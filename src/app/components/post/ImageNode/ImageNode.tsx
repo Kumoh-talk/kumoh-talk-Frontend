8548383eac6e3ebe78f6c7bbcb6341ed7b0b4da3
OhSeChan <seb_s19@naver.com>
2025-03-03 18:00:31 +0900

    feat: tiptap/extension-image에 추가 기능을 적용한 ImageNode 추가

---
+++ src/app/components/post/ImageNode/ImageNode.tsx
@@ -0,0 +1,36 @@
+import { ReactNodeViewRenderer } from '@tiptap/react';
+import ImageComponent from './ImageComponent';
+import Image from '@tiptap/extension-image';
+
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    imageResize: {
+      setImage: (options: {
+        src: string;
+        alt?: string;
+        title?: string;
+        width?: string | number;
+        height?: string | number;
+        isDraggable?: boolean;
+      }) => ReturnType;
+    };
+  }
+}
+
+const CustomImage = Image.extend({
+  name: 'customImage',
+
+  addAttributes() {
+    return {
+      ...this.parent?.(),
+      width: { default: '100%' },
+      height: { default: 'auto' },
+    };
+  },
+
+  addNodeView() {
+    return ReactNodeViewRenderer(ImageComponent);
+  },
+});
+
+export default CustomImage;

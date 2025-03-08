393db3af20c08bc77eb696c23f757d15c73d33ab
OhSeChan <seb_s19@naver.com>
2025-03-08 10:25:17 +0900

    feat: 사용자 추가 정보를 사용한 세미나 신청 폼 초기화 구현

--- src/app/components/apply/SeminarApplyForm/SeminarApplyForm.tsx
+++ src/app/components/apply/SeminarApplyForm/SeminarApplyForm.tsx
@@ -1,29 +1,52 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import { useFormState } from 'react-dom';
 import { FormProvider } from 'react-hook-form';
 import submitAction from '@/app/lib/apis/apply/submitAction';
 import { useFormAction } from '@/app/lib/hooks/apply/useFormAction';
 import FormFields from '../FormFields/FormFields';
-import { submitSeminarApplication } from '@/app/lib/apis/apply/seminarApplication';
-import { departmentValues } from '@/app/lib/constants/apply/selectValues';
-import { getDateForWeekday } from '@/app/lib/utils/dateUtils';
+import {
+  getMyAdditionalInformation,
+  submitSeminarApplication,
+} from '@/app/lib/apis/apply/seminarApplication';
+import { getInitFormValues } from '@/app/lib/utils/post/parseAdditionalInfo';
 import type { SeminarFormValues } from '@/app/lib/schemas/seminarFormSchema';
 import styles from './SeminarApplyForm.module.scss';
 
 const SeminarApplyForm = () => {
   const [formState, formAction] = useFormState(submitAction, null);
+  const [isLoading ,setIsLoading] = useState(true);
+
   const form = useFormAction<SeminarFormValues>({
     formState,
-    defaultValues: {
-      department: departmentValues[0],
-      grade: '1',
-      preferredDate: getDateForWeekday(4),
-    },
     onSuccess: () => {
       submitSeminarApplication(form.getValues());
     },
   });
+
+  useEffect(() => {
+    const initFormValues = async () => {
+      const response = await getMyAdditionalInformation();
+      const { department, studentId, grade, phoneNumber } = response.data;
+
+      const initFormValues = getInitFormValues({
+        department,
+        studentId,
+        grade,
+        phoneNumber,
+      });
+
+      setIsLoading(false);
+      form.reset(initFormValues);
+    };
+
+    initFormValues();
+  }, []);
+
+  if (isLoading) {
+    return <p>로딩 중...</p>;
+  }
 
   return (
     <FormProvider {...form}>

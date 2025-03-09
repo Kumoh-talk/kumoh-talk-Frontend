785e945c5890465c7e985f513c9e8c194a52d81d
oko_jin <dswvgw6412@naver.com>
2025-03-09 19:57:37 +0900

    fix: 댓글 등록 버튼 디바운스 적용
    #69

--- src/app/lib/hooks/useCommentInput.ts
+++ src/app/lib/hooks/useCommentInput.ts
@@ -1,23 +1,26 @@
-import { KeyboardEvent } from 'react';
+import { useTransition } from 'react';
 import useInput from './useInput';
 import { postRecruitmentBoardComment } from '../apis/recruitment-boards/recruitmentBoard';
 
 export default function useCommentInput(boardId: string) {
   const [content, onChange, reset] = useInput({ comment: '' });
+  const [isPending, startTransition] = useTransition();
 
   const onSubmit = () => {
-    if (!content.comment) {
-      return;
-    }
-    const commentBody = {
-      groupId: null,
-      content: content.comment,
-    };
-    postRecruitmentBoardComment(boardId, commentBody);
+    startTransition(async () => {
+      if (!content.comment) {
+        return;
+      }
+      const commentBody = {
+        groupId: null,
+        content: content.comment,
+      };
+      await postRecruitmentBoardComment(boardId, commentBody);
 
-    reset();
-    window.location.reload();
+      reset();
+      window.location.reload();
+    });
   };
 
-  return { comment: content.comment, onChange, reset, onSubmit };
+  return { comment: content.comment, onChange, reset, onSubmit, isPending };
 }

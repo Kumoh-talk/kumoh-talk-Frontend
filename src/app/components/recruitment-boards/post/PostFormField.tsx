7da302911426cc60ad2f385e2a392e3ef05ee4a6
oko_jin <dswvgw6412@naver.com>
2025-03-09 15:32:58 +0900

    fix: 등록하기 버튼 디바운스 적용

--- src/app/components/recruitment-boards/post/PostFormField.tsx
+++ src/app/components/recruitment-boards/post/PostFormField.tsx
@@ -15,83 +15,81 @@ export default function PostFormField({
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { isSubmitting, errors },
     control,
   } = useFormContext();
 
   return (
-    <>
-      <div className={styles.fields}>
-        <div className={styles.row}>
-          <InputField control={control} name='title' label='제목' />
-        </div>
-        <div className={styles.row}>
-          <InputField control={control} name='summary' label='부제목' />
-        </div>
-        <div className={styles.row}>
-          <InputField control={control} name='host' label='주최자' />
-        </div>
-        <div className={styles.row}>
-          <InputField
-            control={control}
-            name='recruitmentTarget'
-            label='모집대상'
-          />
-          <InputField
-            control={control}
-            name='recruitmentNum'
-            label='모집인원'
-            type='number'
-          />
-        </div>
-        <div className={styles.row}>
-          <InputField
-            control={control}
-            name='currentMemberNum'
-            label='현재인원'
-            type='number'
-          />
-        </div>
-        <div className={styles.row}>
-          <InputField
-            control={control}
-            name='recruitmentDeadline'
-            label='모집기한'
-            type='date'
-          />
-        </div>
-        <div className={styles.row}>
-          <InputField
-            control={control}
-            name='activityStart'
-            label='활동시작일'
-            type='date'
-          />
-          <InputField
-            control={control}
-            name='activityFinish'
-            label='활동종료일'
-            type='date'
-          />
-        </div>
-        <div className={styles.row}>
-          <InputField control={control} name='activityCycle' label='활동주기' />
-        </div>
-        <div className={styles.row}>
-          <InputField control={control} name='content' label='세부내용' />
-        </div>
-        <div className={styles.postFormCreate}>
-          <FormLabel>신청폼 만들기</FormLabel>
-          <span className={styles.questionError}>{questionError}</span>
-          <hr />
-          <CreatePostForm />
-        </div>
-        <div className={styles.row}>
-          <Button type='submit' size='medium'>
-            등록하기
-          </Button>
-        </div>
+    <div className={styles.fields}>
+      <div className={styles.row}>
+        <InputField control={control} name='title' label='제목' />
       </div>
-    </>
+      <div className={styles.row}>
+        <InputField control={control} name='summary' label='부제목' />
+      </div>
+      <div className={styles.row}>
+        <InputField control={control} name='host' label='주최자' />
+      </div>
+      <div className={styles.row}>
+        <InputField
+          control={control}
+          name='recruitmentTarget'
+          label='모집대상'
+        />
+        <InputField
+          control={control}
+          name='recruitmentNum'
+          label='모집인원'
+          type='number'
+        />
+      </div>
+      <div className={styles.row}>
+        <InputField
+          control={control}
+          name='currentMemberNum'
+          label='현재인원'
+          type='number'
+        />
+      </div>
+      <div className={styles.row}>
+        <InputField
+          control={control}
+          name='recruitmentDeadline'
+          label='모집기한'
+          type='date'
+        />
+      </div>
+      <div className={styles.row}>
+        <InputField
+          control={control}
+          name='activityStart'
+          label='활동시작일'
+          type='date'
+        />
+        <InputField
+          control={control}
+          name='activityFinish'
+          label='활동종료일'
+          type='date'
+        />
+      </div>
+      <div className={styles.row}>
+        <InputField control={control} name='activityCycle' label='활동주기' />
+      </div>
+      <div className={styles.row}>
+        <InputField control={control} name='content' label='세부내용' />
+      </div>
+      <div className={styles.postFormCreate}>
+        <FormLabel>신청폼 만들기</FormLabel>
+        <span className={styles.questionError}>{questionError}</span>
+        <hr />
+        <CreatePostForm />
+      </div>
+      <div className={styles.row}>
+        <Button type='submit' size='full' disabled={isSubmitting}>
+          등록하기
+        </Button>
+      </div>
+    </div>
   );
 }

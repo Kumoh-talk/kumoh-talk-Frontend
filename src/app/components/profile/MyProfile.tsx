d8c2e1b1e75cb09eb2f9ff8a5337cdb8326ca830
wei756 <wei756fg@gmail.com>
2025-03-17 00:56:49 +0900

    feat: 내 작성글 페이지 조회, 삭제 구현

--- src/app/components/profile/MyProfile.tsx
+++ src/app/components/profile/MyProfile.tsx
@@ -1,11 +1,12 @@
 import MaleUser from '@/app/assets/svg/MaleUser';
 import styles from './profileSideBar.module.scss';
+import Link from 'next/link';
 
 export default function MyProfile() {
   return (
     <div className={styles.row}>
       <MaleUser />
-      <p>내 프로필</p>
+      <Link href="/profile">내 프로필</Link>
     </div>
   );
 }
--- src/app/lib/apis/recruitmentBoards.ts
+++ src/app/lib/apis/recruitmentBoards.ts
@@ -1,7 +1,7 @@
 import {
-  RecruitmentArticle,
   RecruitmentByPageResponse,
   RecruitmentType,
+  RemoveRecruitmentResponse,
 } from '../types/recruitmentBoards/recruitmentBoards';
 
 export const getRecruitmentArticlesByPage = async (
@@ -25,6 +25,66 @@ export const getRecruitmentArticlesByPage = async (
 
     if (response.ok && 'success' in result && 'data' in result) {
       return result.data as RecruitmentByPageResponse;
+    } else {
+      console.error('Failed to fetch data:', result);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+};
+
+export const getMyRecruitmentArticlesByPage = async (
+  category: RecruitmentType = 'MENTORING',
+  page: number = 1,
+  size: number = 15,
+  accessToken: string,
+  refreshToken: string,
+) => {
+  try {
+    const query = new URLSearchParams({
+      recruitmentBoardType: category,
+      size: size.toString(),
+      page: page.toString(),
+      sort: 'createdAt,DESC',
+    });
+    const response = await fetch(
+      `${
+        process.env.NEXT_PUBLIC_BASE_URL
+      }/api/recruitment-boards/my-boards?${query.toString()}`,
+      {
+        headers: {
+          cookie: `accessToken=${accessToken}; refreshToken=${refreshToken}`,
+        },
+      },
+    );
+    const result = await response.json();
+
+    if (response.ok && 'success' in result && 'data' in result) {
+      return result.data as RecruitmentByPageResponse;
+    } else {
+      console.error('Failed to fetch data:', result);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+};
+
+export const removeRecruitmentArticle = async (recruitmentBoardId: number) => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/recruitment-boards/${recruitmentBoardId}`,
+      {
+        method: 'DELETE',
+      },
+    );
+    const result = await response.json();
+
+    if (response.ok) {
+      return { success: true } as RemoveRecruitmentResponse;
     } else {
       console.error('Failed to fetch data:', result);
       return null;

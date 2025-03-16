5c97c85e4d416cbc93d47ab8215fc686619c1f59
OhSeChan <seb_s19@naver.com>
2025-03-16 13:23:55 +0900

    feat: 게시글 작성 상태 관리를 위한 컨텍스트 추가

---
+++ src/app/lib/contexts/post/PostContentContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, useState } from 'react';
+
+interface PostContentContextValue {
+  boardId: number;
+  setBoardId: (boadId: number) => void;
+  title: string;
+  setTitle: (title: string) => void;
+  tagList: string[];
+  setTagList: (tags: string[]) => void;
+  boardHeadImageUrl: string;
+  setBoardHeadImageUrl: (url: string) => void;
+}
+
+const PostContentContext = createContext<PostContentContextValue | undefined>(
+  undefined
+);
+
+export const PostContentProvider = ({ children }: { children: React.ReactNode }) => {
+  const [boardId, setBoardId] = useState(0);
+  const [title, setTitle] = useState('');
+  const [tagList, setTagList] = useState<string[]>([]);
+  const [boardHeadImageUrl, setBoardHeadImageUrl] = useState('');
+
+  return (
+    <PostContentContext.Provider
+      value={{
+        boardId,
+        setBoardId,
+        title,
+        setTitle,
+        tagList,
+        setTagList,
+        boardHeadImageUrl,
+        setBoardHeadImageUrl,
+      }}
+    >
+      {children}
+    </PostContentContext.Provider>
+  );
+};
+
+export const usePostContent = () => {
+  const context = useContext(PostContentContext);
+
+  if (!context) {
+    throw new Error('usePost must be used within a PostContentProvider');
+  }
+
+  return context;
+};

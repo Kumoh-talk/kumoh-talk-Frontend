8baf5851c76e5e0c335acb16cd81b20b379b6879
oko_jin <dswvgw6412@naver.com>
2025-03-17 13:06:57 +0900

    fix: 다시 promise 적용

--- src/app/recruitment-boards/@applicationModal/(.)list/[id]/page.tsx
+++ src/app/recruitment-boards/@applicationModal/(.)list/[id]/page.tsx
@@ -5,18 +5,25 @@ export default async function Page({
   params,
   searchParams,
 }: {
-  params: {
+  params: Promise<{
     id: string;
-  };
-  searchParams: {
+  }>;
+  searchParams: Promise<{
     id: string;
     applicantId: string;
-  };
+  }>;
 }) {
+  const { id: applicantId } = await params;
+  const { id: recruitmentBoardId, applicantId: applicantUserId } =
+    await searchParams;
+
   return (
     <div>
       <Modal modalId='applicationModal'>
-        <ApplicationPage params={params} searchParams={searchParams} />
+        <ApplicationPage
+          params={params}
+          searchParams={searchParams}
+        />
       </Modal>
     </div>
   );

9d293c8f23b6abb457cd8651697b300485e76fa0
oko_jin <dswvgw6412@naver.com>
2025-03-17 13:21:03 +0900

    fix: 모달 페이지에서는 ...props로 넘김

--- src/app/recruitment-boards/@applicationModal/(.)list/[id]/page.tsx
+++ src/app/recruitment-boards/@applicationModal/(.)list/[id]/page.tsx
@@ -1,28 +1,12 @@
 import Modal from '@/app/components/common/modal/Modal';
 import ApplicationPage from '@/app/recruitment-boards/list/[id]/page';
 
-export default async function Page({
-  params,
-  searchParams,
-}: {
-  params: Promise<{
-    id: string;
-  }>;
-  searchParams: Promise<{
-    id: string;
-    applicantId: string;
-  }>;
-}) {
-  const { id: applicantId } = await params;
-  const { id: recruitmentBoardId, applicantId: applicantUserId } =
-    await searchParams;
-
+export default async function Page({...props}: any) {
   return (
     <div>
       <Modal modalId='applicationModal'>
         <ApplicationPage
-          params={params}
-          searchParams={searchParams}
+          {...props}
         />
       </Modal>
     </div>

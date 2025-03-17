0bf1438ad56a08a80b554d6fbb37cefab244dd5a
OhSeChan <seb_s19@naver.com>
2025-03-17 19:12:01 +0900

    feat: confirm 클릭으로 인한 모달 닫힘 방지 적용

--- src/app/components/post/Draft/DraftItem.tsx
+++ src/app/components/post/Draft/DraftItem.tsx
@@ -10,13 +10,25 @@ interface DraftItemProps {
 }
 
 const DraftItem = ({ boardId, updatedAt, title, loadDraft, onDelete }: DraftItemProps) => {
+  const handleDeleteClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+
+    setTimeout(() => {
+      const isConfirmed = window.confirm('임시 저장 글을 정말 삭제하시겠습니까?');
+
+      if (isConfirmed) {
+        onDelete(boardId);
+      }
+    }, 0);
+  };
+
   return (
     <div className={styles.draftItem}>
       <span className={styles.createdAt}>{updatedAt}</span>
       <button className={styles.title} onClick={() => loadDraft(boardId)}>
         {title}
       </button>
-      <button className={styles.deleteBtn} onClick={() => onDelete(boardId)}>
+      <button className={styles.deleteBtn} onClick={handleDeleteClick}>
         <TrashSvg/>
       </button>
     </div>

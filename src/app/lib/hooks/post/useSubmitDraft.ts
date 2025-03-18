31be4b904b734aa05fc0bfc8831df06bedcda4cb
OhSeChan <seb_s19@naver.com>
2025-03-19 07:12:17 +0900

    feat: 임시 저장 글 생성 및 수정 로직 재사용을 위한 커스텀 훅 분리

---
+++ src/app/lib/hooks/post/useSubmitDraft.ts
@@ -0,0 +1,52 @@
+import { useCallback } from 'react';
+import { toast } from 'react-toastify';
+import { useCurrentEditor } from '@tiptap/react';
+import { usePostContent } from '@/app/lib/contexts/post/PostContentContext';
+import { createDraft, editDraft } from '@/app/lib/apis/post/submitDraft';
+
+export const useSubmitDraft = (close: () => void) => {
+  const { boardId, setBoardId, title, tagList, boardHeadImageUrl } = usePostContent();
+  const { editor } = useCurrentEditor();
+
+  const submitDraft = useCallback(async () => {
+    if (!editor) return;
+
+    const handleSuccess = () => {
+      close();
+      toast.success('작성 중인 글이 저장되었습니다.');
+    };
+
+    const handleError = () => toast.error('작성 글 저장에 실패했습니다.');
+
+    if (boardId) {
+      await editDraft(
+        {
+          id: boardId,
+          title,
+          contents: editor.getHTML(),
+          categoryName: tagList,
+          boardHeadImageUrl,
+          editor,
+        },
+        { onSuccess: handleSuccess, onError: handleError }
+      );
+    } else {
+      const newBoardId = await createDraft(
+        {
+          title,
+          contents: editor.getHTML(),
+          categoryName: tagList,
+          boardType: 'SEMINAR',
+          boardHeadImageUrl,
+          editor,
+        },
+        { onSuccess: handleSuccess, onError: handleError }
+      );
+      if (newBoardId) {
+        setBoardId(newBoardId);
+      }
+    }
+  }, [boardId, editor, title, tagList, boardHeadImageUrl, close, setBoardId]);
+
+  return { submitDraft };
+};

09acbcb24d693d2ea3e102e3c10aa4229037a91e
OhSeChan <seb_s19@naver.com>
2025-03-19 00:18:30 +0900

    feat: boardId에 따른 임시 저장 글 생성 및 수정 기능 구현

--- src/app/components/post/Draft/Draft.tsx
+++ src/app/components/post/Draft/Draft.tsx
@@ -1,10 +1,9 @@
-import { usePostContent } from '@/app/lib/contexts/post/PostContentContext';
-import { useCurrentEditor } from '@tiptap/react';
 import { toast } from 'react-toastify';
-import { postDraft } from '@/app/lib/apis/post/boards';
+import { useCurrentEditor } from '@tiptap/react';
+import { usePostContent } from '@/app/lib/contexts/post/PostContentContext';
+import { createDraft, editDraft } from '@/app/lib/apis/post/submitDraft';
 import DraftList from './DraftList';
 import Button from '../../common/button/Button';
-import type { PostBoardsProps } from '@/app/lib/apis/post/boards';
 import styles from './Draft.module.scss';
 
 interface DraftProps {
@@ -12,36 +11,47 @@ interface DraftProps {
 }
 
 const Draft = ({ close }: DraftProps) => {
-  const { title, tagList, boardHeadImageUrl } = usePostContent();
+  const { boardId, setBoardId, title, tagList, boardHeadImageUrl } = usePostContent();
   const { editor } = useCurrentEditor();
 
   const submitDraft = async () => {
     if (!editor) return;
 
-    const draft: PostBoardsProps = {
-      title,
-      contents: editor?.getHTML(),
-      categoryName: tagList,
-      boardType: 'SEMINAR',
-    };
-
-    if (boardHeadImageUrl) {
-      draft.boardHeadImageUrl = boardHeadImageUrl;
-    }
-
-    try {
-      const response = await postDraft(draft);
-      const { success } = response.data;
-
-      if (success === 'false') {
-        toast.error('작성 글 저장에 실패했습니다.');
-        return;
-      }
-
+    const handleSuccess = () => {
       close();
       toast.success('작성 중인 글이 저장되었습니다.');
-    } catch (error) {
-      toast.error('작성 글 저장에 실패했습니다.');
+    };
+
+    const handleError = () => toast.error('작성 글 저장에 실패했습니다.');
+
+    if (boardId) {
+      await editDraft(
+        {
+          id: boardId,
+          title,
+          contents: editor.getHTML(),
+          categoryName: tagList,
+          boardHeadImageUrl,
+          editor,
+        },
+        { onSuccess: handleSuccess, onError: handleError }
+      );
+    } else {
+      const newBoardId = await createDraft(
+        {
+          title,
+          contents: editor.getHTML(),
+          categoryName: tagList,
+          boardType: 'SEMINAR',
+          boardHeadImageUrl,
+          editor,
+        },
+        { onSuccess: handleSuccess, onError: handleError }
+      );
+
+      if (newBoardId) {
+        setBoardId(newBoardId);
+      }
     }
   };
 

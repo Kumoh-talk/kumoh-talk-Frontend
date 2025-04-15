8361792158feb17876ec926fe30e658af1cd9386
oko_jin <dswvgw6412@naver.com>
2025-04-15 23:15:04 +0900

    feat: hls를 통해 m3u8 파일 재생 기능 구현
    #199

--- src/app/components/streaming/[id]/VideoStreaming.tsx
+++ src/app/components/streaming/[id]/VideoStreaming.tsx
@@ -1,9 +1,40 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
 import styles from './videoStreaming.module.scss';
+import Hls from 'hls.js';
 
 export default function VideoStreaming() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const videoUrl = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
+
+  useEffect(() => {
+    if (videoRef.current) {
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(videoUrl);
+        hls.attachMedia(videoRef.current);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+          videoRef.current?.play();
+        });
+
+        return () => {
+          hls.destroy();
+        };
+      } else if (
+        videoRef.current.canPlayType('application/vnd.apple.mpegurl')
+      ) {
+        videoRef.current.src = videoUrl;
+        videoRef.current.addEventListener('loadedmetadata', () => {
+          videoRef.current?.play();
+        });
+      }
+    }
+  }, [videoUrl]);
+
   return (
     <div className={styles.streamingVideo}>
-      <div>스트리밍 탭</div>
+      <video ref={videoRef} className={styles.videoPlayer} />
     </div>
   );
 }

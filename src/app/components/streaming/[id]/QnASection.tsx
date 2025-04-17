965afd0ee4a79be0e12d90b29f36db6e9dc405d3
oko_jin <dswvgw6412@naver.com>
2025-04-17 11:06:18 +0900

    feat: Q&A탭 UI 구현
    #199

--- src/app/components/streaming/[id]/QnASection.tsx
+++ src/app/components/streaming/[id]/QnASection.tsx
@@ -1,15 +1,27 @@
 'use client';
 
+import QnACard from './QnACard';
 import QnAField from './QnAField';
 import styles from './qnaSection.module.scss';
 import { FormProvider, useForm } from 'react-hook-form';
+
+interface Props {
+  qnaList: {
+    qnaId: number;
+    name: string;
+    content: string;
+    time: string;
+    likes: number;
+    isAnswered: boolean;
+  }[];
+}
 
 const defaultValues = {
   content: '',
   isAnonymity: false,
 };
 
-export default function QnASection() {
+export default function QnASection({ qnaList }: Props) {
   const formState = useForm({ defaultValues });
 
   const onSubmit = async (data: unknown) => {
@@ -28,7 +40,11 @@ export default function QnASection() {
           <QnAField />
         </form>
       </FormProvider>
-      <div className={styles.listWrapper}>질문 리스트 나오는 곳</div>
+      <div className={styles.listWrapper}>
+        {qnaList.map((qna) => (
+          <QnACard key={qna.qnaId} {...qna} />
+        ))}
+      </div>
     </div>
   );
 }

8affa1e528426a8a65ecf90125c4d190078088f6
oko_jin <dswvgw6412@naver.com>
2025-04-19 13:27:21 +0900

    feat: vod 리스트 페이지 UI 구현
    #201

---
+++ src/app/vod-list/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from '../components/common/footer/Footer';
+import Header from '../components/common/header/Header';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <div>
+      <Header title='다시보기' />
+      {children}
+      <Footer />
+    </div>
+  );
+}

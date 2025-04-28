8bea934eb0a54c6587d8c4d3f523586d6a96fafb
okojin <dswvgw6412@naver.com>
2025-04-28 21:15:30 +0900

    feat: 채팅 input창 기능 구현

--- src/app/components/streaming/[id]/ChattingInput.tsx
+++ src/app/components/streaming/[id]/ChattingInput.tsx
@@ -1,17 +1,55 @@
 'use client';
 
-import { useContext } from 'react';
+import {
+  ChangeEvent,
+  useContext,
+  useRef,
+  useState,
+  useTransition,
+} from 'react';
 import styles from './chattingInput.module.scss';
 import { SideTabContext } from './SideTabProvider';
+import { Send } from 'lucide-react';
 
 export default function ChattingInput() {
+  const chattingInputRef = useRef<HTMLInputElement | null>(null);
   const { tab } = useContext(SideTabContext);
+  const [content, setContent] = useState('');
+  const [isPending, startTransition] = useTransition();
+
+  const handleChatting = (e: ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  };
+
+  const handleChattingSubmit = () => {
+    startTransition(async () => {
+      setContent('');
+      chattingInputRef.current?.focus();
+      console.log(content);
+    });
+  };
 
   return (
     <>
       {tab === 'chatting' ? (
         <div className={styles.chattingInput}>
-          <input type='text' placeholder='채팅을 입력하세요.' />
+          <input
+            ref={chattingInputRef}
+            type='text'
+            placeholder='채팅을 입력하세요.'
+            name='content'
+            value={content}
+            onChange={handleChatting}
+          />
+          {content && (
+            <button
+              className={styles.sendButton}
+              onClick={handleChattingSubmit}
+              disabled={isPending}
+            >
+              <Send />
+            </button>
+          )}
         </div>
       ) : null}
     </>

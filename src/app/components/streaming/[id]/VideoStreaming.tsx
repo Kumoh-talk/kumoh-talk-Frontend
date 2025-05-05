7ab97fa7be94dcbcb5abfe92de1f6e16d6459a2b
dswvgw6412 <dswvgw6412naver.com>
2025-05-05 19:45:46 +0900

    fix: 영상 url을 state로 관리

--- src/app/components/streaming/[id]/VideoStreaming.tsx
+++ src/app/components/streaming/[id]/VideoStreaming.tsx
@@ -1,20 +1,26 @@
-"use client";
+'use client';
 
-import { useEffect, useRef, useState } from "react";
-import styles from "./videoStreaming.module.scss";
-import Hls, { type Level } from "hls.js";
-import Caption from "./Caption";
-import SubVideoStreaming from "./subVideoStreaming";
+import { useEffect, useRef, useState } from 'react';
+import styles from './videoStreaming.module.scss';
+import Hls, { type Level } from 'hls.js';
+import Caption from './Caption';
+import SubVideoStreaming from './subVideoStreaming';
 
 export default function VideoStreaming() {
+  const [mainScreenUrl, setMainScreenUrl] = useState(
+    'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8'
+  );
+  const [subScreenUrl, setSubScreenUrl] = useState(
+    'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8'
+  );
+
   const videoRef = useRef<HTMLVideoElement>(null);
-  const videoUrl = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
 
   useEffect(() => {
     if (videoRef.current) {
       if (Hls.isSupported()) {
         const hls = new Hls();
-        hls.loadSource(videoUrl);
+        hls.loadSource(mainScreenUrl);
         hls.attachMedia(videoRef.current);
         hls.on(Hls.Events.MANIFEST_PARSED, function () {
           videoRef.current?.play();
@@ -24,25 +30,30 @@ export default function VideoStreaming() {
           hls.destroy();
         };
       } else if (
-        videoRef.current.canPlayType("application/vnd.apple.mpegurl")
+        videoRef.current.canPlayType('application/vnd.apple.mpegurl')
       ) {
-        videoRef.current.src = videoUrl;
-        videoRef.current.addEventListener("loadedmetadata", () => {
+        videoRef.current.src = mainScreenUrl;
+        videoRef.current.addEventListener('loadedmetadata', () => {
           videoRef.current?.play();
         });
       }
     }
-  }, [videoUrl]);
+  }, [mainScreenUrl]);
 
   return (
     <div className={styles.streamingVideo}>
-      <video ref={videoRef} className={styles.videoPlayer} controls muted />
       <div className={styles.overlay}>
         <div className={styles.top}>
-          <SubVideoStreaming />
+          <SubVideoStreaming
+            mainScreenUrl={mainScreenUrl}
+            subScreenUrl={subScreenUrl}
+            setMainScreenUrl={setMainScreenUrl}
+            setSubScreenUrl={setSubScreenUrl}
+          />
         </div>
         <Caption />
       </div>
+      <video ref={videoRef} className={styles.videoPlayer} controls muted />
     </div>
   );
 }

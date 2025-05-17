a0220550329026499c97fdf8e2ff056325e03ba8
dswvgw6412 <dswvgw6412@naver.com>
2025-05-18 00:10:11 +0900

    fix: VOD 플레이 페이지 반응형 작업

--- src/app/vod-list/layout.tsx
+++ src/app/vod-list/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from '../components/common/footer/Footer';
-import Header from '../components/common/header/Header';
+import Footer from "../components/common/footer/Footer";
+import Header from "../components/common/header/Header";
 
 interface Props {
   children: React.ReactNode;
@@ -8,9 +8,8 @@ interface Props {
 export default function Layout({ children }: Props) {
   return (
     <div>
-      <Header title='다시보기' />
+      <Header title="다시보기" />
       {children}
-      <Footer />
     </div>
   );
 }

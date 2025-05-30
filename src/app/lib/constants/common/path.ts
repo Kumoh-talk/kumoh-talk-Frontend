c81666249c597b76fd27e6d2de389a36bb594039
dswvgw6412 <dswvgw6412@naver.com>
2025-05-30 16:55:35 +0900

    fix: 경로 수정

--- src/app/lib/constants/common/path.ts
+++ src/app/lib/constants/common/path.ts
@@ -2,11 +2,15 @@ export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL as string;
 
 export const END_POINTS = {
   PUBLISH: {
-    CREATE_CHAT: (chatId: string) => `/app/streaming/${chatId}`,
+    CREATE_CHAT: (chatId: string) => `/app/streaming/${chatId}/add-chat`,
+    CREATE_QNA: (qnaId: string) => `/app/streaming/${qnaId}/add-qna`,
+    LIKED_QNA: (qnaId: string) => `/app/streaming/${qnaId}/liked-qna`,
+    DELETE_QNA: (qnaId: string) => `/app/streaming/${qnaId}/delete-qna`,
   },
   SUBSCRIBE: {
-    NEW_CHAT: (chatId: string) => `/chat/streaming/${chatId}`,
-    NEW_QNA: (qnaId: string) => `/qna/streaming/${qnaId}`,
+    NEW_CHAT: (chatId: string) => `/chat/streaming/${chatId}/add`,
+    NEW_QNA: (qnaId: string) => `/qna/streaming/${qnaId}/add`,
+    LIKED_QNA: (qnaId: string) => `/qna/streaming/${qnaId}/liked`,
     DELETE_QNA: (qnaId: string) => `/qna/streaming/${qnaId}/delete`,
   },
 };

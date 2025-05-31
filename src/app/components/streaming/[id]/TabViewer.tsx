a2e0d2f3c60e4d208fc49e9e6222734299324094
dswvgw6412 <dswvgw6412@naver.com>
2025-06-01 01:42:55 +0900

    fix: 북마크 세션 제거

--- src/app/components/streaming/[id]/TabViewer.tsx
+++ src/app/components/streaming/[id]/TabViewer.tsx
@@ -4,21 +4,15 @@ import { useContext } from 'react';
 import { SideTabContext } from './SideTabProvider';
 import ChattingList from './ChattingList';
 import QnASection from './QnASection';
-import BookmarkSection from '../../vod-list/[id]/BookmarkSection';
 import { Bookmark } from '@/app/lib/types/streaming/vod';
 
-interface Props {
-  bookmarkList?: Bookmark[];
-}
-
-export default function TabViewer({ bookmarkList }: Props) {
+export default function TabViewer() {
   const { tab, setTab } = useContext(SideTabContext);
 
   return (
     <>
       {tab === 'chatting' && <ChattingList />}
       {tab === 'qna' && <QnASection />}
-      {tab === 'bookmark' && <BookmarkSection bookmarkList={bookmarkList!} />}
     </>
   );
 }

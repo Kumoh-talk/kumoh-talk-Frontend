64bd1759185116210dc1b508f84dc90e0493969c
dswvgw6412 <dswvgw6412@naver.com>
2025-06-02 00:55:20 +0900

    fix: chatList 제거

--- src/app/vod-list/[id]/page.tsx
+++ src/app/vod-list/[id]/page.tsx
@@ -5,114 +5,7 @@ import UtilityTab from '@/app/components/streaming/[id]/UtilityTab';
 import { getVodDetail } from '@/app/lib/apis/vod/vod';
 import BookmarkSection from '@/app/components/vod-list/[id]/BookmarkSection';
 import { VodDetail } from '@/app/lib/types/streaming/vod';
-
-const chatList = [
-  {
-    chatId: 0,
-    socketId: 0,
-    name: '자바스크립트최고',
-    content: 'JPA란 무엇인가요?',
-    time: '02:13',
-  },
-  {
-    chatId: 1,
-    socketId: 1,
-    name: '제이피에이비씨디',
-    content: 'JPA가 너무 궁금했는데 발표 기대할게요',
-    time: '02:14',
-  },
-  {
-    chatId: 2,
-    socketId: 2,
-    name: '파이썬키스트',
-    content: '오렌지주스 마시면서 들어야지',
-    time: '03:51',
-  },
-  {
-    chatId: 3,
-    socketId: 3,
-    name: '자바칩프라푸치노',
-    content: '오늘도 달달한 자바 한잔',
-    time: '03:23',
-  },
-  {
-    chatId: 4,
-    socketId: 4,
-    name: '이런씨플플',
-    content: '자바 너무 어려운 것 같아요',
-    time: '04:11',
-  },
-  {
-    chatId: 5,
-    socketId: 5,
-    name: '러스트라이앵글',
-    content: 'ㅋ',
-    time: '06:23',
-  },
-  {
-    chatId: 6,
-    socketId: 6,
-    name: '카멜레온케이스',
-    content:
-      '자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액',
-    time: '08:20',
-  },
-  {
-    chatId: 7,
-    socketId: 7,
-    name: '코틀리니지',
-    content: '악의적 사용자 제제좀요 ㅠ',
-    time: '08:30',
-  },
-  {
-    chatId: 8,
-    socketId: 6,
-    name: '카멜레온케이스',
-    content:
-      '자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액',
-    time: '08:20',
-  },
-  {
-    chatId: 9,
-    socketId: 6,
-    name: '카멜레온케이스',
-    content:
-      '자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액',
-    time: '08:20',
-  },
-  {
-    chatId: 10,
-    socketId: 6,
-    name: '카멜레온케이스',
-    content:
-      '자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액',
-    time: '08:20',
-  },
-  {
-    chatId: 11,
-    socketId: 6,
-    name: '카멜레온케이스',
-    content:
-      '자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액',
-    time: '08:20',
-  },
-  {
-    chatId: 12,
-    socketId: 6,
-    name: '카멜레온케이스',
-    content:
-      '자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액',
-    time: '08:20',
-  },
-  {
-    chatId: 13,
-    socketId: 6,
-    name: '카멜레온케이스',
-    content:
-      '자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액 자바 어려워 빼애애애애액',
-    time: '08:20',
-  },
-];
+import VideoVod from '@/app/components/vod-list/[id]/VideoVod';
 
 const bookmarkList = [
   {
@@ -141,11 +34,12 @@ export default async function Page({ params }: Props) {
   const { id } = params;
 
   const vodDetail = (await getVodDetail(id)) as VodDetail;
+  console.log(vodDetail);
 
   return (
     <div className={styles.container}>
       <div className={styles.streamingWrapper}>
-        <VideoStreaming
+        <VideoVod
           camUrl={vodDetail.camUrl}
           camTsQuery={vodDetail.camTsQuery}
           slideUrl={vodDetail.slideUrl}

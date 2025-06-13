a6288d8b9c566456908af0e022da7e68640329a0
dswvgw6412 <dswvgw6412@naver.com>
2025-06-14 02:27:40 +0900

    feat: 투표 기능 구독 적용

--- src/app/components/streaming/[id]/SocketProvider.tsx
+++ src/app/components/streaming/[id]/SocketProvider.tsx
@@ -4,6 +4,7 @@ import useChatSubscription from '@/app/lib/hooks/socket/useChatSubscription';
 import useErrorSubscription from '@/app/lib/hooks/socket/useErrorSubscription';
 import useQnaSubscription from '@/app/lib/hooks/socket/useQnaSubscription';
 import useSocketConnect from '@/app/lib/hooks/socket/useSocketConnect';
+import useVoteSubscription from '@/app/lib/hooks/socket/useVoteSubscription';
 
 interface Props {
   paramId: string;
@@ -13,6 +14,7 @@ export default function SocketProvider({ paramId }: Props) {
   useSocketConnect({ streamId: paramId });
   useChatSubscription({ chatId: paramId });
   useQnaSubscription({ qnaId: paramId });
+  useVoteSubscription({ streamId: paramId });
   useErrorSubscription();
 
   return <></>;

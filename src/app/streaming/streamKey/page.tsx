83f23c28248f4789883efa07100431f92551e4c6
dswvgw6412 <dswvgw6412@naver.com>
2025-06-15 15:33:29 +0900

    feat: 어드민 권한을 가진 유저만 접속 가능하도록 구현

--- src/app/streaming/streamKey/page.tsx
+++ src/app/streaming/streamKey/page.tsx
@@ -1,8 +1,19 @@
 import { cookies } from 'next/headers';
 import styles from './page.module.scss';
 import StreamKeyField from '@/app/components/streaming/streamKey/StreamKeyField';
+import { parseJwt } from '@/app/lib/apis/auth';
+import { UserRoleValidator } from '@/app/lib/apis/userRoleValidator';
+import { notFound } from 'next/navigation';
 
 export default function Page() {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get('accessToken')?.value;
+  const userRole = accessToken ? parseJwt(accessToken).USER_ROLE : '';
+
+  if (!UserRoleValidator.admin(userRole)) {
+    notFound();
+  }
+
   return (
     <div className={styles.container}>
       <StreamKeyField />
